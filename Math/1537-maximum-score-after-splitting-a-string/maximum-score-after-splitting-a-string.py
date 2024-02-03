ad2c83a7f2ef0c15e80ae94c05b58b5815d04fbd
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2024-02-03 07:24:01 -0500

    Time: 39 ms (55.33%) | Memory: 16.6 MB (97.96%) - LeetSync

--- Math/1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.py
+++ Math/1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.py
@@ -2,6 +2,8 @@ class Solution:
     def maxScore(self, s: str) -> int:
         if not s:
             return 0
+
+
         left_zeros = [0] * len(s)
         right_ones = [0] * len(s)
 
@@ -20,12 +22,12 @@ class Solution:
                 right_ones[i] = right_ones[i+1]
 
         
-        left_zeros = left_zeros[1:]
-        right_ones = right_ones[:-1]
-        
+        # left_zeros = left_zeros[1:]
+        # right_ones = right_ones[:-1]
+
         out = 0
-        for i in range(len(s)-1):
-            if out < left_zeros[i] + right_ones[i]:
-                out = left_zeros[i] + right_ones[i]
+        for i in range(1, len(s)):
+            if out < left_zeros[i] + right_ones[i-1]:
+                out = left_zeros[i] + right_ones[i-1]
         
         return out
