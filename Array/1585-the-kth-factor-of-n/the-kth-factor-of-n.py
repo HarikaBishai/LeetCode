58b987ecd74771a37422ab45b90e2848a09b02ca
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2024-02-03 06:17:00 -0500

    Time: 30 ms (93.88%) | Memory: 16.7 MB (53.31%) - LeetSync

---
+++ Array/1585-the-kth-factor-of-n/the-kth-factor-of-n.py
@@ -0,0 +1,22 @@
+class Solution:
+    def kthFactor(self, n: int, k: int) -> int:
+        if n == 1 and k == 1:
+            return 1
+        first_factors = []
+        last_factors = []
+        i = 1
+        
+        while i <= n ** 0.5 and len(first_factors) < k:
+            if n%i == 0:
+                first_factors.append(i)
+                if n//i not in first_factors:
+                    last_factors.append(n//i)
+            i += 1
+
+        print(first_factors, last_factors)
+        if len(first_factors) >= k:
+            return first_factors[k-1]
+        elif len(first_factors) + len(last_factors) >= k :
+            return last_factors[len(last_factors) - (k-len(first_factors)-1) -1]
+        else:
+            return -1
