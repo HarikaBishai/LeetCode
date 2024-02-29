9a7e46336d209ababeceeb8d00e8ccc7709ad58c
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2024-02-29 03:52:11 -0500

    Time: 183 ms (69.08%) | Memory: 21.5 MB (12.81%) - LeetSync

--- Tree/2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.py
+++ Tree/2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.py
@@ -10,23 +10,9 @@ class Solution:
             else:
                 mapper[s[i]].append(i)
 
-        print(mapper)       
         out = 0
         for key in mapper.keys():
             indexes = mapper[key]
             if len(indexes) >=2 and indexes[-1] > indexes[0] + 1:
                 out+= len(list(set(s[indexes[0]+1 : indexes[-1]])))
         return out
-        # for l in range(len(s)-2):
-        #     r = len(s) - 1
-        #     while r > l + 1:
-        #         if s[l] == s[r] and s[l] not in visited:
-        #             mapper = list(set(s[l+1: r]))
-        #             for i in range(len(mapper)):
-        #                 new_str = s[l] + "" + mapper[i] + ""+ s[l]
-        #                 if  new_str not in palindromes:
-        #                     palindromes.add(new_str)
-        #             visited.add(s[l])
-        #         r-=1
-                
-        # return len(palindromes)
