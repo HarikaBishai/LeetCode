22ba52f42672e88a871d42473615346d1c292743
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2024-03-14 19:27:47 -0400

    Time: 82 ms (36.86%) | Memory: 16.7 MB (17.03%) - LeetSync

---
+++ Tree/43-multiply-strings/multiply-strings.py
@@ -0,0 +1,32 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        if num1 == "0" or num2 == "0":
+            return "0"
+        
+        if len(num1) < len(num2):
+            num1, num2 = num2, num1
+        
+        multiplier = 1
+        total = 0
+        for i in range(len(num2)-1, -1, -1):
+            curr = int(num2[i])
+            curr_sum = 0
+            carry  = 0
+            curr_multiplier = 1
+            for j in range(len(num1)-1, -1, -1):
+                prod = curr*int(num1[j]) + carry
+                
+                curr_sum += (prod%10)*curr_multiplier 
+                carry = prod //10
+                curr_multiplier *= 10
+                # print( prod, carry, curr_sum)
+
+           
+            if carry:
+                curr_sum = carry * curr_multiplier + curr_sum
+            total += curr_sum*multiplier
+            multiplier *= 10
+
+        return str(total) 
+
+            
