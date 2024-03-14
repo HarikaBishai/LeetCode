5f86816b7405cb55702bac6b1f6efb4254951180
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2024-03-14 00:01:29 -0400

    Time: 54 ms (63.99%) | Memory: 16.6 MB (76.60%) - LeetSync

--- Tree/965-unique-email-addresses/unique-email-addresses.py
+++ Tree/965-unique-email-addresses/unique-email-addresses.py
@@ -5,14 +5,15 @@ class Solution:
                 localName = emailSpitted[0]
                 domainName = emailSpitted[1]
 
-                if '.' in localName:
-                    localName = localName.replace('.', '')
-
                 if '+' in localName:
                     localName = localName[:localName.index('+')]
 
+                if '.' in localName:
+                    localName = localName.replace('.', '')
 
+                
                 return localName + '@' + domainName
+
         out = set()
         for email in emails:
             out.add(parseEmail(email))
