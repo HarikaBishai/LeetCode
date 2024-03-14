68335173b00f297881e952b10b115333c20b1da8
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2024-03-14 04:02:59 -0400

    Time: 49 ms (18.26%) | Memory: 16.5 MB (57.30%) - LeetSync

--- Tree/31-next-permutation/next-permutation.py
+++ Tree/31-next-permutation/next-permutation.py
@@ -1,22 +1,22 @@
 class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
         ptr = -1
 
         for i in range(len(nums)-2, -1, -1):
-            if(nums[i]<nums[i+1]):
-                ptr=i
+            if nums[i] < nums[i+1]:
+                ptr = i
                 break
-        
         if ptr == -1:
             nums[::] = nums[::-1]
             return nums
         
-        for i in range(len(nums)-1, -1, -1):
-            if nums[i]>nums[ptr]:
+        for i in range(len(nums)-1, ptr, -1):
+            if nums[i] > nums[ptr]:
                 nums[i], nums[ptr] = nums[ptr], nums[i]
                 break
-            
         nums[ptr+1:] = nums[ptr+1:][::-1]
-
         return nums
         
