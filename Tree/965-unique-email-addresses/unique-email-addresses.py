65bf124747882ad7b5e099512346c6befeb9674f
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2024-03-13 23:57:38 -0400

    Time: 55 ms (58.22%) | Memory: 16.6 MB (97.68%) - LeetSync

---
+++ Tree/965-unique-email-addresses/unique-email-addresses.py
@@ -0,0 +1,21 @@
+class Solution:
+    def numUniqueEmails(self, emails: List[str]) -> int:
+        def parseEmail(email):
+                emailSpitted = email.split("@")
+                localName = emailSpitted[0]
+                domainName = emailSpitted[1]
+
+                if '.' in localName:
+                    localName = localName.replace('.', '')
+
+                if '+' in localName:
+                    localName = localName[:localName.index('+')]
+
+
+                return localName + '@' + domainName
+        out = set()
+        for email in emails:
+            out.add(parseEmail(email))
+
+
+        return len(out)
