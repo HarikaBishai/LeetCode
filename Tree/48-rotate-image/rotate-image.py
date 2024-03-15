ac9f5b95241f655e0798becc1718292974500e95
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2024-03-14 21:25:46 -0400

    Time: 46 ms (11.32%) | Memory: 16.6 MB (68.92%) - LeetSync

--- Tree/48-rotate-image/rotate-image.py
+++ Tree/48-rotate-image/rotate-image.py
@@ -18,6 +18,10 @@ class Solution:
             for j in range(i+1 ,n):
                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
+
         for i in range(n):
-            matrix[i][:] = matrix[i][::-1]
+            for j in range(n//2):
+                matrix[i][j], matrix[i][n-j-1] = matrix[i][n-j-1], matrix[i][j]
+        # for i in range(n):
+        #     matrix[i][:] = matrix[i][::-1]
         return matrix
