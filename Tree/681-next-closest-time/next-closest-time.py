dbe022dec7e1295cb16c9e71c1c9ddc0992888e2
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2024-03-16 00:29:08 -0400

    Time: 110 ms (5.20%) | Memory: 16.9 MB (10.39%) - LeetSync

---
+++ Tree/681-next-closest-time/next-closest-time.py
@@ -0,0 +1,27 @@
+from datetime import datetime, timedelta
+class Solution:
+    def nextClosestTime(self, time: str) -> str:
+        out = ''
+
+        time_num = set(time.replace(':', ''))
+        print(time_num)
+
+        while True:
+            # time = datetime.strptime(time, '%H:%M')
+            new_time = datetime.strptime(time, '%H:%M') + timedelta(minutes=1)
+
+            new_time_str = new_time.strftime('%H:%M')
+            print(new_time_str.replace(':', ''))
+            new_time_set = list(set(new_time_str.replace(':', '')))
+
+            i = 0
+
+            while i < len(new_time_set):
+                if new_time_set[i] in time_num:
+                    i+=1
+                else:
+                    break
+            if i==len(new_time_set):
+                return new_time_str
+            time = new_time_str
+
