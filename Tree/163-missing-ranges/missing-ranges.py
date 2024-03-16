f7f9192106df83d8fcdf3df041570c9efd640dfa
hbishai <hbishai@iu.edu>
2024-03-15 22:08:05 -0400

    reading input changes added

--- Tree/163-missing-ranges/missing-ranges.py
+++ Tree/163-missing-ranges/missing-ranges.py
@@ -1,6 +1,7 @@
+from collections import *
 class Solution:
-    def findMissingRanges(self, nums: List[int], lower: int, upper: int) -> List[List[int]]:
-        out = []
+    def findMissingRanges(self, nums: list[int], lower: int, upper: int) -> list[list[int]]:
+        out = []1
         n = len(nums)
 
         for i in range(n):
@@ -14,3 +15,14 @@ class Solution:
         
             
         return out
+    
+def main():
+    nums = [int(num) for num in  input('Enter the numbers seperated by space: ').split()]
+    print(nums)
+    lower = int(input('Enter lower: '))
+    upper = int(input('Enter upper: '))
+    solution = Solution()
+    print(solution.findMissingRanges(nums, lower, upper))
+
+if __name__ == '__main__':
+    main()
