d22a098730e8237f8a2b8b86c066658fb7184baf
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2024-03-19 19:13:07 -0400

    Time: 138 ms (66.58%) | Memory: 20.9 MB (23.67%) - LeetSync

--- Sorting/127-word-ladder/word-ladder.py
+++ Sorting/127-word-ladder/word-ladder.py
@@ -1,31 +1,41 @@
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        
         if endWord not in wordList:
             return 0
-
-        graph = collections.defaultdict(list)
+        
+        graph = defaultdict(list)
 
         for word in wordList:
             for j in range(len(word)):
-                pattern = word[:j] + '*' + word[j+1:]
+                pattern = word[:j] + '*' +word[j+1:]
                 graph[pattern].append(word)
 
         print(graph)
-        q = deque([beginWord])
-        visited = set([beginWord])
+
         res = 1
 
-        while q:
 
-            for i in range(len(q)):
-                word = q.popleft()
-                if word == endWord:
-                    return res
-                for j in range(len(word)):
-                    pattern = word[:j] + '*' + word[j+1:]
-                    for nei in graph[pattern]:
-                        if nei not in visited:
-                            visited.add(nei)
-                            q.append(nei)
-            res+=1
-        return 0
+        def bfs():
+            visited = set([beginWord])
+            q = deque([beginWord])
+            res = 1
+            while q:
+
+                for i in range(len(q)):
+                    word = q.popleft()
+
+                    if word == endWord:
+                        return res
+                    
+                    for j in range(len(word)):
+                        pattern =  word[:j] + '*' +word[j+1:]
+                        for nei in graph[pattern]:
+                            if nei not in visited:
+                                q.append(nei)
+                                visited.add(nei)
+                res+=1
+
+            return 0
+        res = bfs()
+        return res
