f2149cf3a252bcbd6ed56cd163af84ca72bb43da
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2024-03-19 21:17:04 -0400

    Time: 82 ms (93.08%) | Memory: 18.7 MB (7.22%) - LeetSync

--- Sorting/210-course-schedule-ii/course-schedule-ii.py
+++ Sorting/210-course-schedule-ii/course-schedule-ii.py
@@ -1,38 +1,34 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        graph = {c:[] for c in range(numCourses)}
-        
-        for crs, pre in prerequisites:
-            graph[crs].append(pre)
-            
-        visited, cycle = set(), set()
-        output = []
-        
-        def dfs(crs):
-            if crs in cycle:
+        graph = {}
+
+        for i in range(numCourses):
+            graph[i] = []
+
+        for csc, pre in prerequisites:
+            graph[csc].append(pre)
+
+        visited , cycle = set(), set()
+        out = []
+
+        def dfs(crc):
+            if crc in cycle:
                 return False
-            if crs in visited:
+            if crc in visited:
                 return True
+
+            visited.add(crc)
+            cycle.add(crc)
             
-            cycle.add(crs)
-            for pre in graph[crs]:
+            for pre in graph[crc]:
                 if dfs(pre) == False:
                     return False
-            visited.add(crs)
-            cycle.remove(crs)
-            output.append(crs)
             
-            return True
-        
-        
-        for c in range(numCourses):
-            if dfs(c) == False:
-                return []
-        return output
-        
-            
-        
-        
-        
-        
-        
+            out.append(crc)
+            cycle.remove(crc)
+
+        for i in range(numCourses):
+            if i not in visited:
+                if dfs(i) == False:
+                    return []
+        return out
