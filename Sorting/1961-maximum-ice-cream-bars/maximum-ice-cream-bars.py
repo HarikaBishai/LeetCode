1017966e12b785186a03b3067060c31fc7f8a6df
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2024-06-06 16:04:26 -0400

    Time: 612 ms (90.02%) | Memory: 30.3 MB (94.92%) - LeetSync

---
+++ Sorting/1961-maximum-ice-cream-bars/maximum-ice-cream-bars.py
@@ -0,0 +1,23 @@
+class Solution:
+    def maxIceCream(self, costs: List[int], coins: int) -> int:
+        
+        maxElement = max(costs)
+
+        countArray = [0]*(maxElement)
+
+        for ele in costs:
+            countArray[ele-1]+=1
+        
+        
+
+        counter = 0
+        for i in range(len(countArray)):
+            if coins>0 and i+1 <= coins:
+                while countArray[i] and coins > 0 and i+1 <= coins:
+                    coins-=i+1
+                    countArray[i]-=1
+                    counter+=1
+            else:
+                break
+        
+        return counter
