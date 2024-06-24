e044481e2e54d062d329da41cf361906d31092b8
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2024-06-24 00:57:01 -0400

    Time: 372 ms (5.05%) | Memory: 20 MB (42.55%) - LeetSync

--- Sorting/200-number-of-islands/number-of-islands.py
+++ Sorting/200-number-of-islands/number-of-islands.py
@@ -1,32 +1,28 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        
-        if not grid:
-            return 0
-        
-        rows, cols = len(grid), len(grid[0])
+        out = 0
+        m = len(grid)
+        n = len(grid[0])
+        print(m, n)
+        visited = [[0 for _ in range(n)] for _ in range(m)]
 
-        islands = 0
-        visited = set()
+        print(visited)
+        def dfs(i, j):
+            visited[i][j] = 1
+            dir = [(-1, 0), (1,0), (0,-1), (0,1)]
 
-
-        def dfs(r,c):
-            visited.add((r,c))
-            
-            directions = [(-1,0),(1,0),(0,1),(0,-1)]
-
-            for dir in directions:
-                new_r = r + dir[0]
-                new_c = c + dir[1]
-                if new_r in range(rows) and new_c in range(cols) and (new_r, new_c) not in visited and grid[new_r][new_c] == '1':
+            for r, c in dir:
+                new_r = i + r
+                new_c = j + c
+                if new_r in range(0, m) and new_c in range(0, n) and grid[i][j] == '1' and not visited[new_r][new_c]:
                     dfs(new_r, new_c)
+
+            
         
-
-
-        for r in range(rows):
-            for c in range(cols):
-                if  grid[r][c] == "1" and (r,c) not in visited:
-                    dfs(r,c)
-                    islands+=1
-
-        return islands
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == '1' and not visited[i][j]:
+                    dfs(i,j)
+                    out+=1
+        return out
+            
