7a66e3954e9ca1e1897a5b75986d5fe09100afd5
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2024-06-27 22:08:59 -0400

    Time: 189 ms (79.22%) | Memory: 30.1 MB (88.77%) - LeetSync

--- Sorting/209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
+++ Sorting/209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
@@ -5,15 +5,11 @@ class Solution:
         curr_sum = 0
         for i in range(len(nums)):
             curr_sum+=nums[i]
-            print(curr_sum, curr_sum >= target)
             while curr_sum >= target:
                 out = min(i-l+1, out)
                 curr_sum-=nums[l]
                 l+=1
-        if out == len(nums) + 1:
-            return 0
-        else:
-            return out
+        return 0 if out == len(nums) + 1 else out
 
 
            
