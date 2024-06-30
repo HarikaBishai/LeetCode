08d001c8881a88575995160312e1d3d41a072868
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2024-06-29 23:42:14 -0400

    Time: 88 ms (61.79%) | Memory: 21.1 MB (65.79%) - LeetSync

---
+++ Sorting/347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -0,0 +1,17 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        counter = Counter(nums)
+        h = []
+        i = 0
+        for key, value in counter.items():
+            if i < k:
+                heapq.heappush(h,(value, key))
+            else:
+                if h[0][0] < value:
+                    heapq.heappushpop(h,(value, key))
+            i+=1 
+        out = []
+        while h:
+            out.append(heapq.heappop(h)[1])
+
+        return out
