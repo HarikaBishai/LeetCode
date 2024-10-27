bb39adb0a5cb34265079f0255117702d4ac6af0c
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2024-10-26 20:34:57 -0400

    Time: 3 ms (91.11%) | Memory: 20.3 MB (93.65%) - LeetSync

--- Sorting/347-top-k-frequent-elements/top-k-frequent-elements.py
+++ Sorting/347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,18 +1,4 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        if len(nums) <= k:
-            return list(set(nums))
-        counter = defaultdict(int)
-        for i in nums:
-            counter[i]+=1
-        
-        hp = []
-        for i,key in enumerate(list(counter.keys())):
-            if i < k:
-                heapq.heappush(hp, (counter[key], key))
-            elif hp[0][0] <= counter[key]:
-                heapq.heapreplace(hp, (counter[key], key))
-        out = []
-        for count, val in hp:
-            out.append(val)
-        return out
+        counter = Counter(nums)
+        return [x for x,y in counter.most_common(k)]
