98e9dd9ec01e063f9b5a6ed09cf33afb2f54b335
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2024-11-15 15:28:01 -0500

    Time: 1 ms (82.13%) | Memory: 17.5 MB (83.90%) - LeetSync

--- Sorting/280-wiggle-sort/wiggle-sort.py
+++ Sorting/280-wiggle-sort/wiggle-sort.py
@@ -3,7 +3,6 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        nums.sort()
 
         flag = False
         for i in range(1, len(nums)):
