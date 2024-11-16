749c03dc0b2270f67dec2cf6ae4788b47fe38d69
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2024-11-16 12:24:41 -0500

    Time: 0 ms (100.00%) | Memory: 16.6 MB (18.95%) - LeetSync

---
+++ Sorting/1069-confusing-number/confusing-number.py
@@ -0,0 +1,27 @@
+class Solution:
+    def confusingNumber(self, n: int) -> bool:
+        if not n: return False
+        rotations= {
+            0:0,
+            1:1,
+            6:9,
+            8:8,
+            9:6
+        }
+
+        rev = 0
+        # n = int(str(n).rstrip('0'))
+        temp = n
+        print(temp)
+        while temp:
+            curr = temp % 10
+            if curr not in rotations:
+                return False
+            
+            rev = rev * 10 + rotations[curr] % 10
+            temp = temp // 10
+        print(n, rev)
+       
+
+        return True if n!=rev else False 
+            
