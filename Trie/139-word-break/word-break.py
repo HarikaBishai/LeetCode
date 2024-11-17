4238898f6e507e375cc5324bb1be47a4729079ea
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2024-11-16 21:53:40 -0500

    Time: 3 ms (69.43%) | Memory: 17.9 MB (5.02%) - LeetSync

--- Trie/139-word-break/word-break.py
+++ Trie/139-word-break/word-break.py
@@ -1,16 +1,37 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.endOfWord = False
+
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         n = len(s)
 
+        root = TrieNode()
+        
+        for word in wordDict:
+            curr = root
+            for c in word:
+                if c not in curr.children:
+                    curr.children[c] = TrieNode()
+                curr = curr.children[c]
+            curr.endOfWord = True
+        
         dp = [False]*len(s)
 
         for i in range(len(s)):
-            for word in wordDict:
-                if i < len(word) -1:
-                    continue
-                
-                if (i == len(word) - 1 or dp[i-len(word)]) and s[i-len(word)+1: i+1] == word:
-                    dp[i] = True
-                    break
-        
+            if i == 0 or dp[i-1]:
+                curr = root
+                for j in range(i,len(s)):
+                    c = s[j]
+                    if c not in curr.children:
+                        break
+                    curr = curr.children[c]
+                    if curr.endOfWord:
+                        dp[j] = True
         return dp[-1]
+                    
+
+
+
+        
