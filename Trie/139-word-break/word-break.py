79038e1951a6878811e01167d12c9116e6701cbd
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2024-11-16 21:08:45 -0500

    Time: 11 ms (6.82%) | Memory: 17 MB (12.08%) - LeetSync

---
+++ Trie/139-word-break/word-break.py
@@ -0,0 +1,18 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        n = len(s)
+
+        mem = {}
+
+        def dp(i):
+            print(mem, i)
+            if i < 0: return True
+            if i in mem: return mem[i]
+            for word in wordDict:
+                if s[i-len(word)+1: i+1] == word and dp(i-len(word)):
+                    mem[i] = True
+                    return True
+            mem[i] = False
+            return False
+        print(mem)
+        return dp(len(s)-1)
