93dc49041570159e502c3634934aa0ec1973395a
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2024-11-22 04:01:12 -0500

    Time: 164 ms (39.94%) | Memory: 32 MB (53.66%) - LeetSync

--- Trie/221-maximal-square/maximal-square.py
+++ Trie/221-maximal-square/maximal-square.py
@@ -7,7 +7,6 @@ class Solution:
 
         dp = [[1 if matrix[j][i] == '1' else 0 for i in range(COLS)] for j in range(ROWS) ]
 
-        print(dp)
         
         for i in range(ROWS-1,-1,-1):
             for j in range(COLS-1, -1, -1):
@@ -22,23 +21,4 @@ class Solution:
                 maxValue = max(maxValue, dp[i][j])
                    
 
-        # def helper(r, c):
-
-        #     if r not in range(ROWS) or c not in range(COLS):
-        #         return 0
-            
-        #     if (r,c) not in cache:
-                
-        #         down = helper(r+1,c)
-        #         right = helper(r, c+1)
-        #         diag = helper(r+1, c+1)
-
-        #         cache[(r,c)] = 0
-
-        #         if matrix[r][c] == '1':
-        #             cache[(r,c)] = 1 + min(down, right, diag)
-        #     return cache[(r,c)]
-     
-
-        # helper(0,0)
         return maxValue ** 2
