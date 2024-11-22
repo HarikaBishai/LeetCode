8d8118d8765b46baf623ce900b5258208cad28fc
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2024-11-22 03:59:19 -0500

    Time: 182 ms (34.37%) | Memory: 32.2 MB (30.43%) - LeetSync

--- Trie/221-maximal-square/maximal-square.py
+++ Trie/221-maximal-square/maximal-square.py
@@ -5,23 +5,40 @@ class Solution:
         COLS = len(matrix[0]) 
         cache = {}
 
-        def helper(r, c):
+        dp = [[1 if matrix[j][i] == '1' else 0 for i in range(COLS)] for j in range(ROWS) ]
 
-            if r not in range(ROWS) or c not in range(COLS):
-                return 0
+        print(dp)
+        
+        for i in range(ROWS-1,-1,-1):
+            for j in range(COLS-1, -1, -1):
+
+                if i+1 < ROWS and j+1 < COLS and dp[i][j] == 1:
+                    dp[i][j] = 1 + min(dp[i+1][j], dp[i][j+1], dp[i+1][j+1])
+        
+
+        maxValue = 0
+        for i in range(ROWS):
+            for j in range(COLS):
+                maxValue = max(maxValue, dp[i][j])
+                   
+
+        # def helper(r, c):
+
+        #     if r not in range(ROWS) or c not in range(COLS):
+        #         return 0
             
-            if (r,c) not in cache:
+        #     if (r,c) not in cache:
                 
-                down = helper(r+1,c)
-                right = helper(r, c+1)
-                diag = helper(r+1, c+1)
+        #         down = helper(r+1,c)
+        #         right = helper(r, c+1)
+        #         diag = helper(r+1, c+1)
 
-                cache[(r,c)] = 0
+        #         cache[(r,c)] = 0
 
-                if matrix[r][c] == '1':
-                    cache[(r,c)] = 1 + min(down, right, diag)
-            return cache[(r,c)]
+        #         if matrix[r][c] == '1':
+        #             cache[(r,c)] = 1 + min(down, right, diag)
+        #     return cache[(r,c)]
      
 
-        helper(0,0)
-        return max(cache.values()) ** 2
+        # helper(0,0)
+        return maxValue ** 2
