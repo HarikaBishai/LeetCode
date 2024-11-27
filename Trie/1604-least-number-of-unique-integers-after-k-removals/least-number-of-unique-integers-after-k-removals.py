e7e8fbc6ab63217bd6eef83d7bbae3398e31c1e5
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2024-11-26 22:18:48 -0500

    Time: 430 ms (27.38%) | Memory: 38.3 MB (6.74%) - LeetSync

--- Trie/1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.py
+++ Trie/1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.py
@@ -4,7 +4,6 @@ class Solution:
 
         minHeap = [(count,key) for key, count in counter.items()]
         heapq.heapify(minHeap)
-        print(minHeap)
         while minHeap and minHeap[0][0] <= k:
             count, val = heapq.heappop(minHeap)
             k -= count
