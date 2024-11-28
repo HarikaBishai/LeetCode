ef74cac03bb35271cdcf31ba3f525ebdfd38581a
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2024-11-28 03:45:05 -0500

    Time: 19 ms (44.24%) | Memory: 20.5 MB (37.73%) - LeetSync

--- Trie/1039-find-the-town-judge/find-the-town-judge.py
+++ Trie/1039-find-the-town-judge/find-the-town-judge.py
@@ -1,17 +1,11 @@
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
         
-
         graph = {i+1: set() for i in range(n)}
-
 
         for a1, b1 in trust:
             graph[a1].add(b1)
         
-        
-                
-
-
         for i in range(1, n+1):
             if graph[i]:
                 continue
