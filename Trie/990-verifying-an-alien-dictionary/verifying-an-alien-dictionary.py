6127925190b9f5c389973d48dc2abe23dfd23121
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2024-11-28 03:17:35 -0500

    Time: 0 ms (100.00%) | Memory: 17.6 MB (16.15%) - LeetSync

---
+++ Trie/990-verifying-an-alien-dictionary/verifying-an-alien-dictionary.py
@@ -0,0 +1,28 @@
+class Solution:
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+        dictionary = {c:i for i, c in enumerate(order)}
+
+
+        def dfs(w1, w2, i):
+            print(i)
+            if i >= len(w1):
+                return True
+            
+            if i >= len(w2):
+                return False
+
+            if dictionary[w1[i]] < dictionary[w2[i]]:
+                return True
+            elif dictionary[w1[i]] > dictionary[w2[i]]:
+                return False
+            else:
+                return dfs(w1, w2, i+1)
+            
+
+
+
+        for i in range(len(words)-1):
+            if not dfs(words[i], words[i+1], 0):
+                return False
+        
+        return True
