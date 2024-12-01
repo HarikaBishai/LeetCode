ac289ca3aa3efa34acb067ec8fe132b261e84a6e
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2024-12-01 15:42:04 -0500

    Time: 1325 ms (30.93%) | Memory: 32.7 MB (38.96%) - LeetSync

--- Trie/416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ Trie/416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -2,17 +2,33 @@ class Solution:
     def canPartition(self, nums: List[int]) -> bool:
         if sum(nums)%2:
             return False
+
+
+
         target = sum(nums)//2
-        dp = set([0])
-        for i in range(len(nums)-1, -1, -1):
-            numSet = set()
 
-            for j in dp:
-                numSet.add(nums[i]+j)
-                numSet.add(j)
-            dp = numSet
-            if target in numSet:
-                return True
+        dp = [[False] * (target+1) for _ in range(len(nums)+1)]
+
+        dp[0][0] = True
+
+        for i in range(1,len(nums)+1):
+            for j in range(target+1):
+                
+                if nums[i-1] <= j:
+                    dp[i][j] = dp[i-1][j] or dp[i-1][j-nums[i-1]]
+                else: 
+                    dp[i][j] = dp[i-1][j]
         
+        return dp[len(nums)][target]
 
-        return False
+        # dp = set([0])
+        # for i in range(len(nums)-1, -1, -1):
+        #     numSet = set()
+        #     for j in dp:
+        #         numSet.add(nums[i]+j)
+        #         numSet.add(j)
+        #     dp = numSet
+        #     if target in numSet:
+        #         return True
+
+        # return False
