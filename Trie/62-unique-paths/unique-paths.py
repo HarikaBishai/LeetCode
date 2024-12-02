b2635939ba7c74c5c237d4e30286ba1575294a46
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2024-12-01 23:13:40 -0500

    Time: 0 ms (100.00%) | Memory: 17.3 MB (5.31%) - LeetSync

--- Trie/62-unique-paths/unique-paths.py
+++ Trie/62-unique-paths/unique-paths.py
@@ -11,18 +11,18 @@ class Solution:
             dp = curr[:]
 
         return dp[n-1]
+
+        # dp = [[0]*n for _ in range(m)]
+
+
+        # for i in range(m):
+        #     dp[i][0] = 1
+        # for i in range(n):
+        #     dp[0][i] = 1
+
         
-        dp = [[0]*n for _ in range(m)]
-
-
-        for i in range(m):
-            dp[i][0] = 1
-        for i in range(n):
-            dp[0][i] = 1
-
-        
-        for i in range(1,m):
-            for j in range(1,n):
-                dp[i][j] = dp[i-1][j]+dp[i][j-1]
-        return dp[m-1][n-1]
+        # for i in range(1,m):
+        #     for j in range(1,n):
+        #         dp[i][j] = dp[i-1][j]+dp[i][j-1]
+        # return dp[m-1][n-1]
 
