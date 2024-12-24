547d303d513e16a0e5cdf3f110a2fccc7ffe1357
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2024-12-23 20:46:36 -0500

    Time: 0 ms (100.00%) | Memory: 17.8 MB (6.19%) - LeetSync

--- Trie/1547-destination-city/destination-city.py
+++ Trie/1547-destination-city/destination-city.py
@@ -1,15 +1,13 @@
 class Solution:
     def destCity(self, paths: List[List[str]]) -> str:
-        graph = defaultdict(list)
-        allNodes = set()
+        
+        nodes = set()
         for u, v in paths:
-            graph[u].append(v)
-            allNodes.add(u)
-            allNodes.add(v)
+            nodes.add(u)
 
         
-        for node in allNodes:
-            if node not in graph:
-                return node
+        for u, v in paths:
+            if v not in nodes:
+                return v
 
         
