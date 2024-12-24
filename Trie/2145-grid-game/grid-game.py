eaca1044e9f27e47478501c6dfa5bda9a5d957b6
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2024-12-24 15:02:52 -0500

    Time: 95 ms (64.31%) | Memory: 29.7 MB (29.80%) - LeetSync

---
+++ Trie/2145-grid-game/grid-game.py
@@ -0,0 +1,36 @@
+class Solution:
+    def gridGame(self, grid: List[List[int]]) -> int:
+        
+
+        total = sum(grid[0])
+        bottom = 0
+        prefix = 0
+        res = float('inf')
+        for i in range(len(grid[0])):
+            prefix += grid[0][i]
+            top = total - prefix
+            secondBot = max(top, bottom)
+            res = min(res, secondBot)
+            bottom += grid[1][i]
+        return res
+
+ 
+
+        prefixRow1 = grid[0].copy()
+        prefixRow2 = grid[1].copy()
+
+
+        for i in range(1,len(grid[0])):
+            prefixRow1[i] += prefixRow1[i-1]
+            prefixRow2[i] += prefixRow2[i-1]
+        res = float('inf')
+
+        
+        for i in range(len(grid[0])):
+            top = prefixRow1[-1] - prefixRow1[i]
+            bottom = prefixRow2[i-1] if i > 0 else 0
+            print(top, bottom)
+            secondBot = max(top, bottom)
+
+            res = min(res, secondBot)
+        return res
