0bfc8c9d803855842bfc1c69b80948d96576a979
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2024-12-23 21:03:53 -0500

    Time: 0 ms (100.00%) | Memory: 17.9 MB (5.13%) - LeetSync

---
+++ Trie/1619-path-crossing/path-crossing.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isPathCrossing(self, path: str) -> bool:
+        coords = set()
+
+        coords.add((0,0))
+        last = (0,0)
+
+        mapping = {
+            'N' : (0, 1),
+            'W' : (-1,0),
+            'S' : (0, -1),
+            'E' : (1, 0)
+        }
+        for dir in path:
+            last_x, last_y = last
+            curr = (last_x + mapping[dir][0], last_y + mapping[dir][1])
+            if curr in coords:
+                return True
+            last = curr
+            coords.add(last)
+
+        print(coords)
+        return False
+
+            
