6e644bd71f3ecba2d75029855ec40ea89aa20aa4
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2024-12-29 17:02:44 -0500

    Time: 148 ms (68.40%) | Memory: 40.7 MB (17.21%) - LeetSync

--- Trie/883-car-fleet/car-fleet.py
+++ Trie/883-car-fleet/car-fleet.py
@@ -1,13 +1,15 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        stk = []
+        zipped = list(zip(position, speed))
+        zipped.sort(key=lambda x : -x[0])
 
-        pairs = list(zip(position, speed))
-        pairs.sort(key=lambda x : -x[0])
-        for p, s in pairs:
-            time = (target - p )/ s
-            stk.append(time)
-            if stk and len(stk) >=2 and stk[-1] <= stk[-2]:
-                stk.pop()
         
+        stk = []
+        for pos, sp in zipped:
+
+            time = (target-pos)/sp
+            stk.append(time)
+            while len(stk) > 1 and  stk[-1] <= stk[-2]:
+                stk.pop()
+                        
         return len(stk)
