0c31dcec1686cbd9f015e5891b67071ff457c339
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2024-12-29 17:04:01 -0500

    Time: 131 ms (88.54%) | Memory: 40.7 MB (16.76%) - LeetSync

--- Trie/883-car-fleet/car-fleet.py
+++ Trie/883-car-fleet/car-fleet.py
@@ -3,13 +3,12 @@ class Solution:
         zipped = list(zip(position, speed))
         zipped.sort(key=lambda x : -x[0])
 
-        
         stk = []
         for pos, sp in zipped:
 
             time = (target-pos)/sp
             stk.append(time)
-            while len(stk) > 1 and  stk[-1] <= stk[-2]:
+            if len(stk) > 1 and  stk[-1] <= stk[-2]:
                 stk.pop()
                         
         return len(stk)
