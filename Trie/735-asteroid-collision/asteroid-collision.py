3088c5114d9db7eff6d362f3b4426cb1f06ea9e3
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2024-12-29 15:45:03 -0500

    Time: 7 ms (59.46%) | Memory: 19 MB (5.30%) - LeetSync

--- Trie/735-asteroid-collision/asteroid-collision.py
+++ Trie/735-asteroid-collision/asteroid-collision.py
@@ -2,23 +2,23 @@ class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
         stk = []
 
-        for num in asteroids:
-            if not stk:
-                stk.append(num)
-                continue
-            curr = num
-            while stk and stk[-1] > 0 and curr < 0:
-                top = stk.pop()
-                if abs(top) < abs(num):
-                    curr = num
-                elif abs(top) > abs(num) :
-                    curr = top
-                else:
-                    curr = None
-                    break
-            if curr:
-                stk.append(curr)
+        for ast in asteroids:
+            if stk and stk[-1] > 0 and ast < 0:
+                while stk and stk[-1] > 0 and ast < 0:
+                    top = stk[-1]
+                    if abs(top) == abs(ast):
+                        stk.pop()
+                        ast = float(inf)
+                        break
+                    elif abs(top) > abs(ast):
+                        ast = float(inf)
+                        break
+                    else:
+                        stk.pop()
+                if ast!= float(inf):
+                    stk.append(ast)
+
+            else:
+                stk.append(ast)
+        
         return stk
-                    
-               
-                
