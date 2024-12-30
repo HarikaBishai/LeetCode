4d6beb044964453a59289c19708fab485cb4c59e
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2024-12-29 23:32:01 -0500

    Time: 166 ms (41.88%) | Memory: 35.4 MB (18.46%) - LeetSync

--- Trie/84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
+++ Trie/84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
@@ -3,15 +3,11 @@ class Solution:
         stk = []
         maxArea = float('-inf')
         for i, height in enumerate(heights):
-            
-            index = i
-                
+            index = i 
             while stk and stk[-1][1] > height:
                 idx, h = stk.pop()
                 maxArea = max(maxArea, h * (i-idx))
                 index = idx
-
-
             stk.append((index,height))
         while stk:
             idx, h = stk.pop()
