f815124191c534edf5561aa2b815961c2f069cc3
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-01-05 19:49:09 -0500

    Time: 39 ms (40.94%) | Memory: 34.7 MB (62.23%) - LeetSync

--- Trie/234-palindrome-linked-list/palindrome-linked-list.py
+++ Trie/234-palindrome-linked-list/palindrome-linked-list.py
@@ -5,32 +5,37 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        fast_ptr, slow_ptr = head , head
-
-        while fast_ptr and fast_ptr.next:
-            fast_ptr = fast_ptr.next.next
-            slow_ptr = slow_ptr.next
-
         
-        def reverse(start):
-            curr = start
-            prev = None
-            next = None
+        def reverse(l1):
+            prev, next = None, None
+
+            curr = l1
             while curr:
                 next = curr.next
                 curr.next = prev
                 prev = curr
                 curr = next
             return prev
-
-        end = reverse(slow_ptr)
         
 
-        start = head
-        while end:
-            if start.val == end.val:
-                start= start.next
-                end = end.next
-            else:
+        def getMid():
+            slow, fast = head, head
+
+            while slow and fast and fast.next:
+                slow = slow.next
+                fast = fast.next.next
+            
+            return slow
+
+        midPtr = getMid()
+        reveseHalf = reverse(midPtr)
+
+
+        curr = head
+
+        while  reveseHalf:
+            if curr.val != reveseHalf.val:
                 return False
+            curr = curr.next
+            reveseHalf = reveseHalf.next
         return True
