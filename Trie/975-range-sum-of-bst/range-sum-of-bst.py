f97378f4dac0d00ed4040edf9e7e6f19572c51ee
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-01-07 15:44:14 -0500

    Time: 0 ms (100.00%) | Memory: 25.2 MB (6.90%) - LeetSync

--- Trie/975-range-sum-of-bst/range-sum-of-bst.py
+++ Trie/975-range-sum-of-bst/range-sum-of-bst.py
@@ -7,23 +7,18 @@
 class Solution:
     def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
         sum = 0
-
-
-        def getSum(root):
+        def dfs(root):
             nonlocal sum
             if not root:
                 return
-            
-            if root.val >= low and root.val<=high:
-                sum+=root.val
-                getSum(root.left)
-                getSum(root.right)
+            if low <= root.val <= high:
+                sum+= root.val
+                dfs(root.left)
+                dfs(root.right)
             elif root.val < low:
-                getSum(root.right)
+                dfs(root.right)
             elif root.val > high:
-                getSum(root.left)
-        getSum(root)
+                dfs(root.left)
+            
+        dfs(root)
         return sum
-
-
-                
