a8b800f8fb2b875e0267c37d824686fa3a49994f
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-01-12 13:47:14 -0500

    Time: 0 ms (100.00%) | Memory: 18.2 MB (6.29%) - LeetSync

--- Trie/46-permutations/permutations.py
+++ Trie/46-permutations/permutations.py
@@ -1,19 +1,22 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         
-        curr = nums[:]
+
+
+
         out = []
-        def dfs(curr, idx):
-            if idx == len(curr):
-                out.append(curr[:])
-            for i in range(idx, len(curr)):
-                curr[idx], curr[i] = curr[i], curr[idx]
-                dfs(curr, idx+1)
-                curr[idx], curr[i] = curr[i], curr[idx]
 
-            # out.append(curr[:])
-            
-        dfs(curr,0)
+        def getPermutate(nums, i):
+            if i == len(nums):
+                out.append(nums[:])
+            for j in range(i,len(nums)):
+                
+                nums[i],nums[j] = nums[j], nums[i]
+                getPermutate(nums, i+1)
+                nums[i],nums[j] = nums[j], nums[i]
+
+        getPermutate(nums, 0)
+
         return out
+        
 
-            
