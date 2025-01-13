d75b2595713e477c64d505a467f5a8b68ebc5202
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-01-12 23:41:15 -0500

    Time: 59 ms (19.91%) | Memory: 19.4 MB (30.92%) - LeetSync

--- Trie/417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
+++ Trie/417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
@@ -2,36 +2,31 @@ class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         ROWS = len(heights)
         COLS = len(heights[0])
-
-        pac = set()
-        atl = set()
+        pac, alt = set(), set()
 
 
-        def dfs(r,c,visit, prev):
+        def dfs(r,c, visit, height):
+            if r not in range(ROWS) or c not in range(COLS) or (r,c) in visit or heights[r][c] < height:
+                return
             visit.add((r,c))
+            dfs(r+1,c, visit, heights[r][c])
+            dfs(r-1,c, visit, heights[r][c])
+            dfs(r,c+1, visit, heights[r][c])
+            dfs(r,c-1, visit, heights[r][c])
 
-            directions = [(-1,0),(1,0),(0,-1),(0,1)]
-
-            for i , j in directions:
-                new_r = r + i
-                new_c = c + j
-
-                if new_r in range(ROWS) and new_c in range(COLS) and (new_r, new_c) not in visit and heights[new_r][new_c] >= prev:
-                    dfs(new_r,new_c, visit, heights[new_r][new_c])
 
         for c in range(COLS):
-            dfs(0, c , pac, heights[0][c])
-            dfs(ROWS-1, c, atl, heights[ROWS-1][c])
+            dfs(0, c, pac, heights[0][c])
+            dfs(ROWS-1, c, alt, heights[ROWS-1][c])
 
         for r in range(ROWS):
-            dfs(r, 0 , pac, heights[r][0])
-            dfs(r, COLS-1 , atl, heights[r][COLS-1])
+            dfs(r, 0, pac, heights[r][0])
+            dfs(r, COLS-1, alt, heights[r][COLS-1])
 
-        result = []
-        print(pac, atl)
+        res = []
         for i in range(ROWS):
             for j in range(COLS):
-                if (i,j) in pac and (i,j) in atl:
-                    result.append([i,j])
-
-        return result
+                if (i,j) in pac and (i,j) in alt:
+                    res.append((i,j))
+        return res
+        
