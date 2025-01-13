81040b7b5dd05062f40fb900f6b027fa5c54242c
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-01-13 02:00:48 -0500

    Time: 1 ms (69.99%) | Memory: 18.4 MB (15.19%) - LeetSync

--- Trie/684-redundant-connection/redundant-connection.py
+++ Trie/684-redundant-connection/redundant-connection.py
@@ -1,37 +1,36 @@
+class DSU:
+    def __init__(self, n):
+        self.rank = [0 for _ in range(n)]
+        self.parent = [i for i in range(n)]
+
+    def find(self, i):
+        if self.parent[i]!=i:
+            self.parent[i] = self.find(self.parent[i])
+        return self.parent[i]
+    def union(self, x, y):
+        xrep = self.find(x)
+        yrep = self.find(y)
+
+        if xrep == yrep:
+            return True
+        
+        xrank = self.rank[xrep]
+        yrank = self.rank[yrep]
+
+        if xrank <= yrank:
+            self.parent[xrep] = yrep
+            self.rank[yrep]+=1
+        else:
+            self.parent[yrep] = xrep
+            self.rank[xrep]+=1
+        return False
+        
+
+
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        n = len(edges)
-        parent = [i for i in range(n+1)]
+        graph = DSU(len(edges))
 
-        rank = [1]*(n+1)
-
-
-        def find(i):
-            if parent[i]==i:
-                return i
-            parent[i] = find(parent[i])
-            return parent[i]
-
-        def union(u, v):
-
-            urep = find(u)
-            vrep = find(v)
-
-            if urep == vrep:
-                return False
-            urank = rank[urep]
-            vrank = rank[vrep]
-
-            if urank < vrank:
-                parent[urep] = vrep
-                rank[vrep] += 1
-            else:
-                parent[vrep] = urep
-                rank[urep] += 1
-            return True
-
-
-        for u, v in edges:
-            if not union(u,v):
+        for u , v in edges:
+            if graph.union(u-1,v-1):
                 return [u,v]
-        return []
