99d6b5bc6365d2df82c3ab6fd32d9c4adfc6d429
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-01-13 00:28:40 -0500

    Time: 5 ms (41.48%) | Memory: 20 MB (6.73%) - LeetSync

--- Trie/210-course-schedule-ii/course-schedule-ii.py
+++ Trie/210-course-schedule-ii/course-schedule-ii.py
@@ -1,25 +1,37 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        graph = {i: [] for i in range(numCourses)}
-        indegree = [0] * numCourses
+        graph = {i:[] for i in range(numCourses)}
+
+
+
         for v, u in prerequisites:
             graph[u].append(v)
-            indegree[v]+=1
 
-        q = deque()
-        finish = 0
-        for i,val in enumerate(indegree):
-            if val == 0:
-                q.append(i)
-
-        result = []
-        while q:
-            node = q.popleft()
-            result.append(node)
-            for nei in graph[node]:
-                indegree[nei]-=1
-                if indegree[nei] == 0:
-                    q.append(nei)
-
-        return result if len(result) == numCourses else []
         
+
+        visit = set()
+        path = set()
+
+        out = []
+
+        def dfs(i):
+            
+            visit.add(i)
+            path.add(i)
+            for nei in graph[i]:
+                if nei not in visit:
+                    if dfs(nei):
+                        return True
+                elif nei in path:
+                    return True
+
+            path.remove(i)
+            out.append(i)
+            return False
+        
+
+        for v in range(numCourses):
+            if v not in visit and dfs(v):
+                return []
+        
+        return out[::-1]
