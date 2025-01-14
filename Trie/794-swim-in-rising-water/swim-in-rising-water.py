e2137b078f31514c3e6cd754674d146a52ed6b02
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-01-13 19:05:52 -0500

    Time: 39 ms (37.08%) | Memory: 18.7 MB (11.41%) - LeetSync

--- Trie/794-swim-in-rising-water/swim-in-rising-water.py
+++ Trie/794-swim-in-rising-water/swim-in-rising-water.py
@@ -1,24 +1,24 @@
 class Solution:
     def swimInWater(self, grid: List[List[int]]) -> int:
         
+        dir = [(1,0),(-1,0),(0,1),(0,-1)]
+
         N = len(grid)
 
-
-        visited = set([(0,0)])
         h = [(grid[0][0],0,0)]
+        visited = set()
+        visited.add((0,0))
 
         while h:
-            elevation, r,c = heapq.heappop(h)
+            t, r, c = heapq.heappop(h)
 
-            if r == N-1 and c== N-1:
-                return elevation
+            if r == N-1 and c == N-1:
+                return t
+            
+            for i, j in dir:
+                new_r = r + i
+                new_c = c + j
 
-            directions = [(-1,0),(1,0),(0,1),(0,-1)]
-
-            for i , j in directions:
-                new_r = r+i
-                new_c = c+j
-                if new_r in range(N) and new_c in range(N) and (new_r, new_c) not in visited:
-                    heapq.heappush(h,(max(elevation, grid[new_r][new_c]), new_r, new_c))
+                if new_r in range(N) and new_c in range(N) and  (new_r, new_c) not in visited:
+                    heapq.heappush(h, (max(t, grid[new_r][new_c]), new_r, new_c))
                     visited.add((new_r, new_c))
-        return -1
