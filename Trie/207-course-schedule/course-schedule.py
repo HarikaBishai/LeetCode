e6a78eee5844633bca9396d45efd32ba88dd201f
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-01-30 17:18:28 -0500

    Time: 10 ms (19.54%) | Memory: 19.1 MB (50.70%) - LeetSync

--- Trie/207-course-schedule/course-schedule.py
+++ Trie/207-course-schedule/course-schedule.py
@@ -1,61 +1,34 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         
-        # using khan's algo for topology sort
+        indegree = {i: 0 for i in range(numCourses)}
+        graph = {i:[] for i in range(numCourses)}
 
-        graph = {i: [] for i in range(numCourses)}
-        indegree = [0] * numCourses
         for v, u in prerequisites:
             graph[u].append(v)
+
             indegree[v]+=1
-
+        visited = set()
         q = deque()
-        finish = 0
-        for i,val in enumerate(indegree):
-            if val == 0:
-                q.append(i)
+        for node in indegree:
+            if indegree[node] == 0:
+                q.append(node)
+                visited.add(node)
 
 
-        while q:
+        print(q, visited)
+        while q and numCourses:
             node = q.popleft()
-            finish+=1
+
+            
+            numCourses -= 1
+
             for nei in graph[node]:
-                indegree[nei]-=1
+                indegree[nei] -= 1
                 if indegree[nei] == 0:
                     q.append(nei)
-
-        return True if finish == numCourses else False
-        
-        
-
-
-        # using dfs graph cycle detection
-        
-        graph = {i: [] for i in range(numCourses)}
-
-        for v, u in prerequisites:
-            graph[u].append(v)
+                    visited.add(nei)
+        print(numCourses)
+        return True if numCourses == 0 else False
 
 
-
-        path = set()
-        visited = set()
-
-        def dfs(i):
-
-            visited.add(i)
-            path.add(i)
-
-            for nei in graph[i]:
-                if nei not in visited:
-                    if not dfs(nei):
-                        return False
-                elif nei in path:
-                    return False
-            path.remove(i)
-            return True
-
-        for i in range(numCourses):
-            if i not in visited and not dfs(i):
-                return False
-        return True
