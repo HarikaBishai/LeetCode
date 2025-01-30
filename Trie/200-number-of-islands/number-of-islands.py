5c18af53729ee912ca303a0f004dd1a5d4155ec4
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-01-30 01:19:42 -0500

    Time: 323 ms (9.58%) | Memory: 28.2 MB (10.84%) - LeetSync

--- Trie/200-number-of-islands/number-of-islands.py
+++ Trie/200-number-of-islands/number-of-islands.py
@@ -2,32 +2,35 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         ROWS = len(grid)
         COLS = len(grid[0])
+        visited = set()
 
-        visit = set()
+
         no_of_islands = 0
+
         def dfs(r,c):
+            visited.add((r,c))
 
-            visit.add((r,c))
 
-
-            dir = [(-1,0),(1,0),(0,1),(0,-1)]
+            dir = [(-1,0), (1, 0),(0,1),(0,-1)]
 
             for i, j in dir:
-                new_r = r+i
-                new_c = c+j
+                new_r = i + r
+                new_c = j + c
 
-                if new_r in range(ROWS) and new_c in range(COLS) and (new_r, new_c) not in visit and grid[new_r][new_c] == "1":
+                if new_r in range(ROWS) and new_c in range(COLS) and (new_r, new_c) not in visited and grid[new_r][new_c] == '1':
                     dfs(new_r, new_c)
-        
+
+
+            
 
         for i in range(ROWS):
             for j in range(COLS):
-                if (i,j) not in visit and grid[i][j] == "1":
+                if grid[i][j] == '1' and (i,j) not in visited:
                     dfs(i,j)
-                    no_of_islands+=1
-                
-        
+                    no_of_islands +=1
         return no_of_islands
 
-        
+    
+
+
 
