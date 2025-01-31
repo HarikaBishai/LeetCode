7af49fe6fa5393216026f3cad18b00ba0155d304
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-01-30 22:36:08 -0500

    Time: 2 ms (74.88%) | Memory: 17.8 MB (49.81%) - LeetSync

--- Trie/139-word-break/word-break.py
+++ Trie/139-word-break/word-break.py
@@ -19,10 +19,6 @@ class Solution:
                     if start+len(word)<= len(s) and  s[start: start+len(word)] in wordDict and start+len(word) not in seen:
                         q.append(start+len(word))
                         seen.add(start+len(word))
-                # for end in range(start, len(s)):
-                #     if s[start: end+1] in wordDict and end+1 not in seen:
-                #         q.append(end+1)
-                #         seen.add(end+1)
 
         return False
 
