5cb774ae7841c251e570c773e9f3adef31f367b8
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-01-30 23:19:31 -0500

    Time: 0 ms (100.00%) | Memory: 17.7 MB (70.30%) - LeetSync

---
+++ Trie/140-word-break-ii/word-break-ii.py
@@ -0,0 +1,25 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        
+
+
+        path = []
+
+        out = []
+        def dfs(i):
+
+            if i == len(s):
+                out.append(" ".join(path))
+                
+            
+
+            for word in wordDict:
+                if i + len(word) <= len(s) and s[i : i + len(word)] == word:
+                    path.append(word)
+                    dfs(i + len(word))
+                    path.pop()
+            return False
+
+        dfs(0)
+
+        return out
