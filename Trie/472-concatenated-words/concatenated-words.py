32a4362e3d7f397e42360ccb17bcedd7b60b443d
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-01-31 00:35:21 -0500

    Time: 280 ms (33.55%) | Memory: 20 MB (73.72%) - LeetSync

--- Trie/472-concatenated-words/concatenated-words.py
+++ Trie/472-concatenated-words/concatenated-words.py
@@ -14,35 +14,26 @@ class Solution:
             return dp[-1]
 
 
-            # q = deque([0])
-
-
-            # wordDict = set(wordDict)
-            # seen = set([0])
-
-            # while q:
-            #     for _ in range(len(q)):
-            #         start = q.popleft()
-
-            #         if start == len(s):
-            #             return True
-
-
-            #         for word in wordDict:
-            #             if start+len(word)<= len(s) and  s[start: start+len(word)] in wordDict and start+len(word) not in seen:
-            #                 q.append(start+len(word))
-            #                 seen.add(start+len(word))
-
-            # return False
-
+        wordDict = set(words)
         out = []
         minLen = len(words[0])
 
         for word in words:
             if len(word) < minLen:
                 minLen = len(word)
-        for i, word in enumerate(words):
 
-            if len(word) >= minLen * 2 and checkConcatenation(set(words), word):
+        for word in words:
+
+            dp = [False]*(len(word)+1)
+            dp[0]= True
+            
+
+            for i in range(1, len(word)+1):
+                for j in range(i):
+                    if dp[j] and word[j:i] in wordDict and word[j:i] != word:
+                        dp[i] = True
+                        break
+            if dp[-1]:
                 out.append(word)
+
         return out
