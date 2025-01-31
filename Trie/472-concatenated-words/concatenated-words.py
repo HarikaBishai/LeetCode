582e3bf76741ebea60fbdf3fd910bc2257119079
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-01-31 00:35:38 -0500

    Time: 279 ms (34.30%) | Memory: 19.8 MB (81.30%) - LeetSync

--- Trie/472-concatenated-words/concatenated-words.py
+++ Trie/472-concatenated-words/concatenated-words.py
@@ -1,26 +1,10 @@
 class Solution:
     def findAllConcatenatedWordsInADict(self, words: List[str]) -> List[str]:
         
-        def checkConcatenation(wordDict, s):
-            dp = [False]*(len(s)+1)
-            dp[0]= True
-            
-
-            for i in range(1, len(s)+1):
-                for j in range(i):
-                    if dp[j] and s[j:i] in wordDict and s[j:i] != s:
-                        dp[i] = True
-                        break
-            return dp[-1]
-
 
         wordDict = set(words)
         out = []
-        minLen = len(words[0])
-
-        for word in words:
-            if len(word) < minLen:
-                minLen = len(word)
+     
 
         for word in words:
 
