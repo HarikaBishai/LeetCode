ffd6b196bd12157ff77b63e083ae0d733579f2b8
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-02-01 16:18:11 -0500

    Time: 27 ms (45.21%) | Memory: 19.5 MB (22.75%) - LeetSync

--- Trie/803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
+++ Trie/803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
@@ -2,15 +2,40 @@ class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
         
 
-        dp = [float('inf')]*n
-        dp[src] = 0
+        # dp = [float('inf')]*n
+        # dp[src] = 0
 
-        for i in range(k+1):
-            new_dp = dp.copy()
+        # for i in range(k+1):
+        #     new_dp = dp.copy()
 
-            for u, v , w in flights:
-                if dp[u] != float('inf') and dp[u]+w < new_dp[v]:
-                    new_dp[v] = dp[u]+w
-            dp = new_dp
+        #     for u, v , w in flights:
+        #         if dp[u] != float('inf') and dp[u]+w < new_dp[v]:
+        #             new_dp[v] = dp[u]+w
+        #     dp = new_dp
 
-        return -1 if dp[dst] == float('inf') else dp[dst]
+        # return -1 if dp[dst] == float('inf') else dp[dst]
+
+
+
+        graph = defaultdict(list)
+
+        for u, v , w in flights:
+            graph[u].append((v,w))
+
+        heap = [(0, src, 0)]
+
+        best = dict()
+
+
+        while heap:
+            dist, city, stops = heapq.heappop(heap)
+            if city == dst:
+                return dist
+            
+            if stops <= k:
+                for nei, w in graph[city]:
+                    if (nei, stops+1) not in best or best[(nei, stops+1)] > dist+w:
+                        best[(nei, stops+1)] = dist+w
+                        heapq.heappush(heap, (dist+w, nei, stops+1))
+
+        return -1
