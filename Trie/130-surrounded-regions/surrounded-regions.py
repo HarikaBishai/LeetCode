c7ab1438ec60f644cc3a31e87b98c4e6f5a856e5
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-02-02 04:47:45 -0500

    Time: 5 ms (70.86%) | Memory: 21.9 MB (39.31%) - LeetSync

--- Trie/130-surrounded-regions/surrounded-regions.py
+++ Trie/130-surrounded-regions/surrounded-regions.py
@@ -7,35 +7,29 @@ class Solution:
 
         ROWS = len(board)
         COLS = len(board[0])
-        q = deque()
-        seen = set()
-        for i in range(ROWS):
-            if board[i][0] == "O":
-                q.append((i,0))
-            if board[i][COLS-1] == "O":
-                q.append((i,COLS-1))
+        # q = deque()
 
-        for i in range(1,COLS):
-            if board[0][i] == "O":
-                q.append((0,i))
-            if board[ROWS-1][i] == "O":
-                q.append((ROWS-1,i))
-
-        while q:
-            
-            r, c = q.popleft()
-            if (r,c) in seen:
-                continue
+        def dfs(r,c):
             board[r][c] = 'T'
-            seen.add((r,c))
             dir = [(-1, 0), (0, -1), (1,0), (0,1)]
 
             for i , j in dir:
                 new_r = i+ r
                 new_c = j+ c
-                if new_r in range(ROWS) and new_c in range(COLS) and (new_r, new_c) not in seen and board[new_r][new_c] == 'O':
-                    q.append((new_r, new_c))
+                if new_r in range(ROWS) and new_c in range(COLS) and board[new_r][new_c] == 'O':
+                    dfs(new_r, new_c)
         
+        for i in range(ROWS):
+            if board[i][0] == "O":
+                dfs(i,0)
+            if board[i][COLS-1] == "O":
+                dfs(i,COLS-1)
+
+        for i in range(1,COLS):
+            if board[0][i] == "O":
+                dfs(0,i)
+            if board[ROWS-1][i] == "O":
+                dfs(ROWS-1,i)
 
         for i in range(ROWS):
             for j in range(COLS):
@@ -45,4 +39,42 @@ class Solution:
             for j in range(COLS):
                 if board[i][j] == 'T':
                     board[i][j] = 'O'
+
+        # seen = set()
+        # for i in range(ROWS):
+        #     if board[i][0] == "O":
+        #         q.append((i,0))
+        #     if board[i][COLS-1] == "O":
+        #         q.append((i,COLS-1))
+
+        # for i in range(1,COLS):
+        #     if board[0][i] == "O":
+        #         q.append((0,i))
+        #     if board[ROWS-1][i] == "O":
+        #         q.append((ROWS-1,i))
+
+        # while q:
+            
+        #     r, c = q.popleft()
+        #     if (r,c) in seen:
+        #         continue
+        #     board[r][c] = 'T'
+        #     seen.add((r,c))
+        #     dir = [(-1, 0), (0, -1), (1,0), (0,1)]
+
+        #     for i , j in dir:
+        #         new_r = i+ r
+        #         new_c = j+ c
+        #         if new_r in range(ROWS) and new_c in range(COLS) and (new_r, new_c) not in seen and board[new_r][new_c] == 'O':
+        #             q.append((new_r, new_c))
+        
+
+        # for i in range(ROWS):
+        #     for j in range(COLS):
+        #         if board[i][j] == 'O':
+        #             board[i][j] = 'X'
+        # for i in range(ROWS):
+        #     for j in range(COLS):
+        #         if board[i][j] == 'T':
+        #             board[i][j] = 'O'
         
