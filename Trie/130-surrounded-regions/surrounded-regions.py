2ababfd9ac3d452e6e1f6f526ae33c7208cacffc
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-02-02 04:42:13 -0500

    Time: 11 ms (31.38%) | Memory: 21.9 MB (39.31%) - LeetSync

--- Trie/130-surrounded-regions/surrounded-regions.py
+++ Trie/130-surrounded-regions/surrounded-regions.py
@@ -15,7 +15,7 @@ class Solution:
             if board[i][COLS-1] == "O":
                 q.append((i,COLS-1))
 
-        for i in range(COLS):
+        for i in range(1,COLS):
             if board[0][i] == "O":
                 q.append((0,i))
             if board[ROWS-1][i] == "O":
@@ -24,15 +24,14 @@ class Solution:
         while q:
             
             r, c = q.popleft()
-            if (r,c) in seen:
-                continue
+            
             board[r][c] = 'T'
-            seen.add((r,c))
+            
             dir = [(-1, 0), (0, -1), (1,0), (0,1)]
             for i , j in dir:
                 new_r = i+ r
                 new_c = j+ c
-                if new_r in range(ROWS) and new_c in range(COLS) and (new_r, new_c) not in seen and board[new_r][new_c] == 'O':
+                if new_r in range(ROWS) and new_c in range(COLS) and board[new_r][new_c] == 'O':
                     q.append((new_r, new_c))
         
         for i in range(ROWS):
