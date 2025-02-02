757730cb1f0ee7aacd22f638976b374ec2f10ac6
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-02-02 17:14:33 -0500

    Time: 7 ms (51.90%) | Memory: 19.8 MB (7.93%) - LeetSync

--- Trie/323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.py
+++ Trie/323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.py
@@ -1,39 +1,27 @@
+ 
+
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
-        parent = [i for i in range(n)]
 
-        rank = [1]*n
-
-        components = n
-
-        def find(i):
-            if parent[i]==i:
-                return i
-            parent[i] = find(parent[i])
-            return parent[i]
-
-        def union(u, v):
-            nonlocal components
-
-            urep = find(u)
-            vrep = find(v)
-
-            if urep == vrep:
-                return
-            urank = rank[urep]
-            vrank = rank[vrep]
-
-            if urank < vrank:
-                parent[urep] = vrep
-                rank[vrep] += 1
-                components-=1
-            else:
-                parent[vrep] = urep
-                rank[urep] += 1
-                components-=1
-            return
-
+        graph = {u: [] for u in range(n)}
 
         for u, v in edges:
-            union(u,v)
+            graph[u].append(v)
+            graph[v].append(u)
+
+        visited = set()
+        def dfs(node):
+            
+            visited.add(node)
+
+            for nei in graph[node]:
+                if nei not in visited:
+                    dfs(nei)
+        
+        components = 0
+        for i in range(n):
+            if i not in visited:
+                components+=1
+                dfs(i)
         return components
+        
