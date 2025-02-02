3845fef262b9b2876e2ce5a34a0208ef046a1bbb
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-02-02 02:35:16 -0500

    Time: 1 ms (56.54%) | Memory: 17.7 MB (61.70%) - LeetSync

--- Trie/269-alien-dictionary/alien-dictionary.py
+++ Trie/269-alien-dictionary/alien-dictionary.py
@@ -1,43 +1,56 @@
 class Solution:
     def alienOrder(self, words: List[str]) -> str:
         
-        graph = {c:set() for word in words for c in word }
+        if len(words) == 1: return "".join(list(set(words[0])))
+        
+        graph = defaultdict(list)
+        
+        
+        dictionary = ""
+        for i in range(1, len(words)):
+            word1 = words[i-1]
+            word2 = words[i]
+            dictionary += word1 + word2
+            n1 = len(word1)
+            n2 = len(word2)
+            minLen = min(n1, n2)
 
-
-        for i in range(len(words)-1):
-            w1, w2 = words[i], words[i+1]
-
-            minLength = min(len(w1), len(w2))
-
-            if w1[:minLength] == w2[:minLength] and len(w1) > len(w2):
+            if word1[:minLen] == word2[:minLen] and n1 > n2:
                 return ""
-
-            for j in range(minLength):
-                if w1[j]!=w2[j]:
-                    graph[w1[j]].add(w2[j])
+            
+            for j in range(minLen):
+                
+                if word1[j]!=word2[j]:
+                    graph[word1[j]].append(word2[j])
                     break
 
-        visited = set()
-        path = set()
-        res = []
+        dictionary = set(list(dictionary))
+        indegree = { v:0 for v in dictionary}
 
-        def dfs(i):
-            visited.add(i)
-            path.add(i)
+        for u in graph:
+            for v in graph[u]:
+                indegree[v]+=1
 
-            for nei in graph[i]:
-                if nei not in visited and dfs(nei):
-                    return True
-                elif nei in path:
-                    return True
-            path.remove(i)
-            res.append(i)
-            return False
-        for c in graph.keys():
-            if c not in visited:
-               if dfs(c):
-                return ""
-        return "".join(res[::-1])
+        q = deque()
+        seen = set()
+        for u in indegree:
+            if indegree[u] == 0:
+                q.append(u)
+        out = []
 
-                
+        while q:
+            c = q.popleft()
+
+            out.append(c)
+
+            for nei in graph[c]:
+                indegree[nei]-=1
+                if indegree[nei] == 0:
+                    q.append(nei)
         
+        return "".join(out) if len(out) == len(dictionary) else ""
+
+
+
+
+       
