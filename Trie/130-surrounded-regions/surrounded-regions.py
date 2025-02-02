7aabf79fdc8d4ed8aab989f663a59340d9822a16
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-02-02 04:39:59 -0500

    Time: 7 ms (60.92%) | Memory: 21.7 MB (63.68%) - LeetSync

--- Trie/130-surrounded-regions/surrounded-regions.py
+++ Trie/130-surrounded-regions/surrounded-regions.py
@@ -5,44 +5,43 @@ class Solution:
         """
         
 
-
         ROWS = len(board)
         COLS = len(board[0])
-
-
-        def dfs(r,c ):
-            if r not in range(ROWS) or c not in range(COLS) or board[r][c] != 'O':
-                return
-            board[r][c] ='T'
-            dfs(r+1,c )
-            dfs(r-1,c )
-            dfs(r,c+1 )
-            dfs(r,c-1 )
-
-
+        q = deque()
+        seen = set()
         for i in range(ROWS):
-            if board[i][0] == 'O':
-                dfs(i, 0)
-            if board[i][COLS-1] == 'O':
-                dfs(i, COLS-1)
-        for c in range(COLS):
-            if board[0][c] == 'O':
-                dfs(0,c)
-            if board[ROWS-1][c] == 'O':
-                dfs(ROWS-1, c) 
+            if board[i][0] == "O":
+                q.append((i,0))
+            if board[i][COLS-1] == "O":
+                q.append((i,COLS-1))
+
+        for i in range(COLS):
+            if board[0][i] == "O":
+                q.append((0,i))
+            if board[ROWS-1][i] == "O":
+                q.append((ROWS-1,i))
+
+        while q:
             
-            
+            r, c = q.popleft()
+            if (r,c) in seen:
+                continue
+            board[r][c] = 'T'
+            seen.add((r,c))
+            dir = [(-1, 0), (0, -1), (1,0), (0,1)]
+
+            for i , j in dir:
+                new_r = i+ r
+                new_c = j+ c
+                if new_r in range(ROWS) and new_c in range(COLS) and (new_r, new_c) not in seen and board[new_r][new_c] == 'O':
+                    q.append((new_r, new_c))
+        
         for i in range(ROWS):
             for j in range(COLS):
                 if board[i][j] == 'O':
                     board[i][j] = 'X'
-        
-
         for i in range(ROWS):
             for j in range(COLS):
                 if board[i][j] == 'T':
                     board[i][j] = 'O'
-
-
-         
-
+        
