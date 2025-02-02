4207ab6592969f33cc956112a22bbbaae8e37772
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-02-02 05:04:14 -0500

    Time: 65 ms (16.54%) | Memory: 19.8 MB (8.45%) - LeetSync

--- Trie/417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
+++ Trie/417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
@@ -8,7 +8,7 @@ class Solution:
         pac_set = set()
         alt_set = set()
 
-        def dfs(r,c, seen, prev):
+        def dfs(r,c, seen):
             seen.add((r,c))
 
             dir = [(-1,0),(0,-1),(0,1),(1,0)]
@@ -17,15 +17,15 @@ class Solution:
                 new_c = c + j
 
                 if new_r in range(ROWS) and new_c in range(COLS) and heights[new_r][new_c] >= heights[r][c]  and (new_r, new_c) not in seen:
-                    dfs(new_r, new_c, seen, heights[new_r][new_c])
+                    dfs(new_r, new_c, seen)
 
         for i in range(ROWS):
-            dfs(i, 0, pac_set, -1)
-            dfs(i, COLS-1, alt_set, -1)
+            dfs(i, 0, pac_set)
+            dfs(i, COLS-1, alt_set)
 
         for i in range( COLS):
-            dfs(0, i, pac_set, -1)
-            dfs(ROWS-1, i, alt_set, -1)
+            dfs(0, i, pac_set)
+            dfs(ROWS-1, i, alt_set)
 
         print(pac_set, alt_set)
         return list(pac_set & alt_set)
