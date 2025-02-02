776b66c13c950983f20c4d16d31d59766a6ce2d6
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-02-02 03:26:14 -0500

    Time: 494 ms (5.05%) | Memory: 19 MB (56.30%) - LeetSync

---
+++ Trie/1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
@@ -0,0 +1,33 @@
+class Solution:
+    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+        
+
+        ROWS = len(grid)
+        COLS = len(grid[0])
+
+        if grid[0][0] == 1 or grid[ROWS-1][COLS-1] == 1:
+            return -1
+
+
+        best = {(0,0): 1}
+        
+        h = [(1,0,0)]
+
+        while h:
+            dist, r, c = heapq.heappop(h)
+            
+           
+            if r == ROWS-1 and c == COLS-1:
+                return dist
+            
+            dir = [(-1,0), (1,0), (0,-1), (0, 1), (1, -1),(1, 1), (-1, 1), (-1, -1)]
+
+            for i, j in dir:
+                new_r = r + i
+                new_c = c + j
+
+                if new_r in range(ROWS) and new_c in range(COLS) and grid[new_r][new_c] == 0 and ((new_r, new_c) not in best):
+                    best[(new_r, new_c)] = dist+1
+
+                    heapq.heappush(h, (dist+1, new_r, new_c))
+        return -1
