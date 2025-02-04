2a6dae86f0f7da3ab5b4749a831b2a15060aabec
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-02-03 20:00:38 -0500

    Time: 87 ms (21.82%) | Memory: 24.9 MB (24.48%) - LeetSync

--- Trie/286-walls-and-gates/walls-and-gates.py
+++ Trie/286-walls-and-gates/walls-and-gates.py
@@ -3,12 +3,15 @@ class Solution:
         """
         Do not return anything, modify rooms in-place instead.
         """
-
-        q = deque()
+        
 
         ROWS = len(rooms)
         COLS = len(rooms[0])
 
+
+
+
+        q = deque()
         visited = set()
         for i in range(ROWS):
             for j in range(COLS):
@@ -16,29 +19,20 @@ class Solution:
                     q.append((i,j))
                     visited.add((i,j))
 
-        dist = 0
-
-        def addRoom(r, c, q):
-            if r in range(ROWS) and c in range(COLS) and (r,c) not in visited and rooms[r][c] != -1:
-                visited.add((r,c))
-                q.append((r,c))
-
+        level = 1
         while q:
             for _ in range(len(q)):
                 r, c = q.popleft()
-                rooms[r][c] = dist
-            
-                addRoom(r+1, c, q)
-                addRoom(r-1, c, q)
-                addRoom(r, c+1, q)
-                addRoom(r, c-1, q)
-            dist +=1
+                
+                dir = [(-1,0),(1,0),(0,1),(0,-1)]
+                for i, j in dir:
+                    new_r = r+i
+                    new_c = c+j
+                    if new_r in range(ROWS) and new_c in range(COLS) and (new_r, new_c) not in visited and rooms[new_r][new_c] != -1:
+                        q.append((new_r, new_c))
+                        rooms[new_r][new_c] = level
+                        visited.add((new_r, new_c))
+            level+=1
 
-        return dist
+        return rooms
 
-
-
-
-            
-            
-        
