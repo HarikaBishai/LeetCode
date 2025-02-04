8401d60e26ff97116bc57985df4e76708bb2928d
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-02-04 18:56:04 -0500

    Time: 415 ms (84.58%) | Memory: 42.8 MB (68.37%) - LeetSync

--- Trie/1250-longest-common-subsequence/longest-common-subsequence.py
+++ Trie/1250-longest-common-subsequence/longest-common-subsequence.py
@@ -1,30 +1,28 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        n1 = len(text1)
+        n2 = len(text2)
+
+        dp = [[0] * (n2) for _ in range(n1)]
         
-        m = len(text1)
-        n = len(text2)
-
-
-        dp = [0]*(n+1)
-        for i in range(1,m+1):
-            curr = dp[:]
-            for j in range(1, n+1):
-                if text1[i-1] == text2[j-1]:
-                    curr[j] = 1 + dp[j-1]
-                else:
-                    curr[j] = max(curr[j-1], dp[j])
-            dp = curr[:]
-        return dp[n]
-                
-        dp = [[0]*(n+1) for _ in range(m+1)]
-
-        
-        for i in range(1,m+1):
-            for j in range(1,n+1):
-
-                if text1[i-1] == text2[j-1]:
-                    dp[i][j] = 1 + dp[i-1][j-1]
+        for i in range(n1):
+            for j in range(n2):
+                if text1[i] == text2[j]:
+                    if i == 0 or j == 0:
+                        dp[i][j] = 1
+                    else:
+                        dp[i][j] = 1 + dp[i-1][j-1]
                 else:
                     dp[i][j] = max(dp[i-1][j], dp[i][j-1])
-                    
-        return dp[m][n]
+        return dp[n1-1][n2-1]
+        # def lcs(i, j):
+        #     if i >= n1 or j >= n2:
+        #         return 0
+
+        #     if text1[i] == text2[j]:
+        #         return 1 + lcs(i+1, j+1)
+            
+        #     return max(lcs(i+1, j), lcs(i, j+1))
+
+        return lcs(0,0)
+        
