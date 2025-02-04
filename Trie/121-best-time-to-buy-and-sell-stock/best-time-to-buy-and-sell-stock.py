0b28c92ff32b7b1e2d9e69cd9c37f1a2e4906da3
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-02-04 00:43:07 -0500

    Time: 140 ms (14.59%) | Memory: 26.9 MB (65.36%) - LeetSync

--- Trie/121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ Trie/121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,12 +1,12 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        maxP = 0
+        
+        n = len(prices)
+        maxValtoRight = prices[-1]
 
+        maxProfit = 0
 
-        minBuy = prices[0]
-
-        for p in prices:
-            if p < minBuy:
-                minBuy = p
-            maxP = max(maxP, p - minBuy)
-        return maxP
+        for i in range(n-1, -1, -1):
+            maxValtoRight = max(maxValtoRight, prices[i])
+            maxProfit = max(maxValtoRight-prices[i], maxProfit)
+        return maxProfit
