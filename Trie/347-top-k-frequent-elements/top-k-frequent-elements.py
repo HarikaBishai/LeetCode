603d522167d0c085a5bc63b4dd76099f2478f2e4
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-02-03 23:18:38 -0500

    Time: 3 ms (91.76%) | Memory: 21.3 MB (55.55%) - LeetSync

--- Trie/347-top-k-frequent-elements/top-k-frequent-elements.py
+++ Trie/347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,16 +1,5 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        counter = Counter(nums)
+        count = Counter(nums)
 
-
-        len_buckets = [[] for i in range(len(nums))]
-
-        for key, val in counter.items():
-            len_buckets[val-1].append(key)
-
-        out = []
-        for i in range(len(nums)-1, -1, -1):
-            for num in len_buckets[i]:
-                out.append(num)
-                if len(out) == k:
-                    return out
+        return heapq.nlargest(k, count.keys(), key=count.get)
