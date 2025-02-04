be847245c26c69dce39b2c66b8fec0ed3d209942
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-02-04 01:30:30 -0500

    Time: 3086 ms (7.78%) | Memory: 17.7 MB (73.39%) - LeetSync

--- Trie/567-permutation-in-string/permutation-in-string.py
+++ Trie/567-permutation-in-string/permutation-in-string.py
@@ -1,29 +1,19 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        count_s2 = {}
-        count_s1 = Counter(s1)
-
-        target_s = set()
-
+        n1 = len(s1)
+        n2 = len(s2)
+        if n1 > n2:
+            return False
+        # counter_s1 = counter(s1)
+        s1 = list(s1)
+        s1.sort()
         l = 0
-        for r in range(len(s2)):
-            count_s2[s2[r]] = 1 + count_s2.get(s2[r],0)
-            if s2[r] in count_s1:
-                while count_s2[s2[r]] > count_s1[s2[r]]:
-                    count_s2[s2[l]]-=1
-                    if count_s2[s2[l]] < count_s1[s2[l]] and s2[l] in target_s:
-                        target_s.remove(s2[l])
-                    l+=1
-
-                if count_s1[s2[r]] == count_s2[s2[r]]:
-                    target_s.add(s2[r])
-                if len(target_s) == len(count_s1):
-                    return True
-            else:
-                count_s2 = {}
-                target_s = set()
-                l = r+1
+        for i in range(len(s2)-n1+1):
+            curr = list(s2[i: i+n1])
+            curr.sort()
+            if s1 == curr:
+                return True 
         return False
-            
 
 
+        
