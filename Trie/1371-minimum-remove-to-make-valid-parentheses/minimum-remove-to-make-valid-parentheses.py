c3a03a3bf30cde7c4db26a7f4f3a07e2ffe01e8a
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-02-06 19:43:25 -0500

    Time: 55 ms (27.70%) | Memory: 19.4 MB (41.23%) - LeetSync

--- Trie/1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
+++ Trie/1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
@@ -3,22 +3,18 @@ class Solution:
         
        
         stk = []
-        balance = 0
         indexes_to_remove = []
         for i,c in enumerate(s):
             if c not in '()':
                 continue
             if c == '(':
                 stk.append(i)
-                balance +=1
             elif c == ')':
-                if balance == 0:
+                if len(stk) == 0:
                     indexes_to_remove.append(i)
                 else:
-                    balance-=1
                     stk.pop()
             
-        print(stk)
         indexes_to_remove += stk
 
         indexes_to_remove = set(indexes_to_remove)
