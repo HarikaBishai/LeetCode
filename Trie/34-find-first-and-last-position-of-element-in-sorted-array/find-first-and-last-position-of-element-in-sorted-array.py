32953d7f3833ed02de6eb670d57e5fae8a9f3213
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-02-06 21:04:11 -0500

    Time: 0 ms (100.00%) | Memory: 19.3 MB (18.77%) - LeetSync

--- Trie/34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ Trie/34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,38 +1,36 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-
-
-        if not nums or nums[0]>target or nums[-1]<target: return [-1,-1]
-        leftBound = float('inf')
-
-
-        l = 0
-        r = len(nums)-1
-
-        while l <= r:
-            m = (l+r)//2
-            if nums[m] >= target:
-                leftBound = min(leftBound, m)
-                r=m-1
-            elif nums[m] < target:
-                l=m+1
         
-
-        if nums[leftBound]!=target: return [-1,-1]
-
-        rightBound = -1
-
+        firstPos = -1
+        lastPos = -1
 
         l = 0
-        r = len(nums)-1
+        r = len(nums) -1
 
-        while l <= r:
+        while l<=r:
             m = (l+r)//2
-            if nums[m] <= target:
-                rightBound = max(rightBound, m)
-                l=m+1
-            elif nums[m] > target:
-                r=m-1
 
-        return [leftBound, rightBound]
+            if nums[m] >= target:
+                firstPos = m
+                r = m-1
+            
+            else:
+                l = m+1
+        
+        if firstPos == -1 or nums[firstPos] != target:
+            return [-1, -1]
+
+        l = 0
+        r = len(nums) -1
+
+        while l<=r:
+            m = (l+r)//2
+
+            if nums[m] <= target:
+                rightPos = m
+                l = m+1
+            
+            else:
+                r = m-1
+        return [firstPos, rightPos]
         
