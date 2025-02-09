a6b7f64587e9f5c725d34018bd88c9cddf1fdb30
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-02-08 19:10:34 -0500

    Time: 0 ms (100.00%) | Memory: 18 MB (8.21%) - LeetSync

--- Trie/957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.py
+++ Trie/957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.py
@@ -1,18 +1,14 @@
 class Solution:
     def minAddToMakeValid(self, s: str) -> int:
-        open = 0
         balance = 0
-
-        i = 0
         out = 0
-        while i < len(s):
-            if s[i] == '(':
+        for c in s:
+            if c == '(':
                 balance+=1
             else:
                 if balance:
                     balance-=1
                 else:
                     out+=1
-            i+=1
         out += balance
         return out
