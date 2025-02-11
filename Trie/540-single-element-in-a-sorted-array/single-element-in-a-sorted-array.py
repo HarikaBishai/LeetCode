0f20fe986d7fa814ef6888002e3f52439e29a8de
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-02-11 14:49:37 -0500

    Time: 0 ms (100.00%) | Memory: 24.8 MB (43.83%) - LeetSync

--- Trie/540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
+++ Trie/540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
@@ -1,27 +1,27 @@
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        if nums[0] != nums[1]:
+            return nums[0]
+        if nums[-1]!= nums[-2]:
+            return nums[-1]
+
         l = 1
         r = len(nums)-2
-        if len(nums)==1:
-            return nums[0]
-        if nums[0]!=nums[1]:
-            return nums[0]
-        if nums[-1]!=nums[-2]:
-            return nums[-1]
         while l<=r:
-            mid = (l+r)//2
-            
-            if nums[mid-1]!= nums[mid] and nums[mid+1]!=nums[mid]:
-                return nums[mid]
-            if mid % 2 == 0:
-                if nums[mid]!=nums[mid+1]:
-                    r=mid-1
-                else:
-                    l=mid+1
+            m = (l+r)//2
 
-            else:
-                if nums[mid-1]!=nums[mid]:
-                    r=mid-1
+            if nums[m]!= nums[m-1] and nums[m]!= nums[m+1]:
+                return nums[m]
+            if m%2 == 0:
+                if nums[m] != nums[m+1]:
+                    r = m-1
                 else:
-                    l = mid+1
+                    l = m+1
+            else:
+                if nums[m] != nums[m-1]:
+                    r = m-1
+                else:
+                    l = m+1
         return -1
