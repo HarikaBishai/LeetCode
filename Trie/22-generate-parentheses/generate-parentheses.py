fb0e07199180617edec67590e78ffb8ea9d7b7be
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-02-11 00:02:09 -0500

    Time: 0 ms (100.00%) | Memory: 17.9 MB (86.75%) - LeetSync

--- Trie/22-generate-parentheses/generate-parentheses.py
+++ Trie/22-generate-parentheses/generate-parentheses.py
@@ -1,17 +1,14 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
+        
         open = 0
         close = 0
-
-
-        out = []
         stk = []
-
-
+        out = []
         def dfs(open, close, stk):
-            if open == n and close == n:
+            if open == close == n:
                 out.append("".join(stk))
-            
+                return
             if open < n:
                 stk.append('(')
                 dfs(open+1, close, stk)
@@ -20,6 +17,6 @@ class Solution:
                 stk.append(')')
                 dfs(open, close+1, stk)
                 stk.pop()
-                
-        dfs(0,0, stk)
+            return
+        dfs(0,0,stk)
         return out
