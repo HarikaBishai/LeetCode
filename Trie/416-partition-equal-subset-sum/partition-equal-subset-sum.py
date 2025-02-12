40f011595303ee6f4a4f7634e9e4a87e309b3412
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-02-12 01:18:46 -0500

    Time: 1442 ms (30.08%) | Memory: 33.5 MB (40.95%) - LeetSync

--- Trie/416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ Trie/416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -1,34 +1,38 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        if sum(nums)%2:
+        n = len(nums)
+        total_sum = sum(nums)
+        if total_sum % 2:
             return False
 
+        subset_sum = total_sum//2
 
+        dp = [[False]*(subset_sum+1) for _ in range(n+1)]
 
-        target = sum(nums)//2
+        
 
-        dp = [[False] * (target+1) for _ in range(len(nums)+1)]
-
+        
         dp[0][0] = True
 
-        for i in range(1,len(nums)+1):
-            for j in range(target+1):
-                
-                if nums[i-1] <= j:
-                    dp[i][j] = dp[i-1][j] or dp[i-1][j-nums[i-1]]
-                else: 
+        for i in range(1, n+1):
+            for j in  range(subset_sum+1):
+
+                if nums[i-1] > j:
                     dp[i][j] = dp[i-1][j]
+                else:
+                    dp[i][j] = dp[i-1][j] or dp[i-1][j-nums[i-1]]
+                
+        return dp[n][subset_sum]
+
+
+        def dfs(sum, i):
+            if sum == 0:
+                return True
+            if i <= 0 or sum < 0:
+                return False
+            if nums[i-1] > sum:
+                return dfs(sum, i-1)
+            return dfs(sum-nums[i-1], i-1) or dfs(sum, i-1)
         
-        return dp[len(nums)][target]
+        return dfs(int(total_sum/2), n)
 
-        # dp = set([0])
-        # for i in range(len(nums)-1, -1, -1):
-        #     numSet = set()
-        #     for j in dp:
-        #         numSet.add(nums[i]+j)
-        #         numSet.add(j)
-        #     dp = numSet
-        #     if target in numSet:
-        #         return True
-
-        # return False
