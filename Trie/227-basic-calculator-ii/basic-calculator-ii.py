c4ea27e3651c39ece30fd14681847127a2a086f0
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-02-11 22:46:13 -0500

    Time: 64 ms (67.55%) | Memory: 21.9 MB (22.90%) - LeetSync

--- Trie/227-basic-calculator-ii/basic-calculator-ii.py
+++ Trie/227-basic-calculator-ii/basic-calculator-ii.py
@@ -1,29 +1,31 @@
 class Solution:
     def calculate(self, s: str) -> int:
+        
         stk = []
+        
 
+
+        numbers = set([str(i) for i in range(10)])
+        operations = set(["-", "+", "/", "*"])
+
+        curr_num = 0
         operation = '+'
-        curr_char = 0
-
-        for i, char in enumerate(s):
-          
-            if  char.isdigit():
-                curr_char = (curr_char * 10 )+ int(char)
-            if (char!= " " and (not char.isdigit())) or i==len(s)-1:
-                if operation == '+':
-                    stk.append(curr_char)
-                elif operation == '-':
-                    stk.append(-curr_char)
-                elif operation == '*':
-                    top = stk.pop()
-                    stk.append(top * curr_char)
+        for i, c in enumerate(s):
+            if c in numbers:
+                curr_num = curr_num * 10 + int(c)
+                
+            if c in operations or i == len(s) - 1:
+                if operation == '-':
+                    stk.append(-curr_num)
+                elif operation == '+':
+                    stk.append(curr_num)
                 elif operation == '/':
-                    top = stk.pop()
-                    stk.append(int(top / curr_char))
-                operation = char
-                curr_char = 0
-        out = 0
+                    stk[-1] = int(stk[-1] / curr_num)
+                else:
+                    stk[-1] = stk[-1] * curr_num
+                
+                curr_num = 0
+                operation = c
+        return sum(stk)
 
-        while stk:
-            out+= stk.pop()
-        return out
+
