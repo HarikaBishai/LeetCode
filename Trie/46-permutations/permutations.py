6ac75d88929bcaec3b2e3dcde06ba973d0c9c674
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-02-14 15:18:48 -0500

    Time: 3 ms (33.48%) | Memory: 18.2 MB (14.31%) - LeetSync

--- Trie/46-permutations/permutations.py
+++ Trie/46-permutations/permutations.py
@@ -2,21 +2,19 @@ class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         
 
-
-
+        stk = []
         out = []
+        path = set()
+        def dfs():
+            if len(stk) == len(nums):
+                out.append(stk.copy())
 
-        def getPermutate(nums, i):
-            if i == len(nums):
-                out.append(nums[:])
-            for j in range(i,len(nums)):
-                
-                nums[i],nums[j] = nums[j], nums[i]
-                getPermutate(nums, i+1)
-                nums[i],nums[j] = nums[j], nums[i]
-
-        getPermutate(nums, 0)
-
+            for num in nums:
+                if num not in path:
+                    path.add(num)
+                    stk.append(num)
+                    dfs()
+                    stk.pop()
+                    path.remove(num)
+        dfs()
         return out
-        
-
