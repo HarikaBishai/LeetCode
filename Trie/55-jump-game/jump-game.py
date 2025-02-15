9ae6b9e361990962cf173cffb447bf8aed24ed17
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-02-15 18:46:30 -0500

    Time: 844 ms (17.01%) | Memory: 18.8 MB (17.85%) - LeetSync

--- Trie/55-jump-game/jump-game.py
+++ Trie/55-jump-game/jump-game.py
@@ -12,6 +12,6 @@ class Solution:
             if dp[i] :
                 if i + nums[i] >= n-1:
                     return True
-                for j in range(i,i+nums[i]+1):
+                for j in range(i+1,i+nums[i]+1):
                     dp[j] = True
         return dp[n-1]
