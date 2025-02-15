6f41cc95341d599fd5e6f3af5561c8d9ba16caea
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-02-14 19:14:39 -0500

    Time: 27 ms (19.95%) | Memory: 17.9 MB (66.05%) - LeetSync

--- Trie/10-regular-expression-matching/regular-expression-matching.py
+++ Trie/10-regular-expression-matching/regular-expression-matching.py
@@ -1,8 +1,5 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-        
-
-
 
         def check_match(m, n):
             if n == 0:
@@ -18,8 +15,10 @@ class Solution:
             if p[n-1] == '*' and n > 1:
 
                 zero_match = check_match(m, n-2)
+                if zero_match: return True
                 one_or_more_match = (s[m-1] == p[n-2] or p[n-2] == '.') and check_match(m-1, n)
-                return zero_match or one_or_more_match
+                return one_or_more_match
             return False
+
 
         return check_match(len(s), len(p))
