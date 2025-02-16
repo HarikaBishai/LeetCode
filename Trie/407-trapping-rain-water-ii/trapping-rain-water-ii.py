38d9ab4b3d2bb0db566d80dc15d038b53f0a1914
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-02-16 18:02:50 -0500

    Time: 199 ms (28.15%) | Memory: 19.5 MB (99.81%) - LeetSync

--- Trie/407-trapping-rain-water-ii/trapping-rain-water-ii.py
+++ Trie/407-trapping-rain-water-ii/trapping-rain-water-ii.py
@@ -3,14 +3,12 @@ class Solution:
         ROWS = len(heightMap)
         COLS = len(heightMap[0])
 
-        visited = set()
         boundary = []
         for i in range(ROWS):
             for j in range(COLS):
                 if i == 0 or i == ROWS-1 or j == 0 or j == COLS-1:
                     heapq.heappush(boundary, (heightMap[i][j], i,j))
-                    visited.add((i,j))
-
+                    heightMap[i][j] = -1
         
         total_water = 0
 
@@ -20,11 +18,11 @@ class Solution:
             for i, j in dir:
                 nei_r = i + r
                 nei_c = j + c
-                if nei_r in range(ROWS) and nei_c in range(COLS) and (nei_r, nei_c) not in visited:
+                if nei_r in range(ROWS) and nei_c in range(COLS) and heightMap[nei_r][nei_c]!=-1:
                     nei_h = heightMap[nei_r][nei_c]
                     if nei_h < minHeight:
                         total_water += minHeight - nei_h
                     
                     heapq.heappush(boundary, (max(minHeight, nei_h), nei_r, nei_c) )
-                    visited.add((nei_r, nei_c))
+                    heightMap[nei_r][nei_c] = -1
         return total_water
