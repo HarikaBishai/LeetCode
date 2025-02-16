29d05bed2f3e00ec4c9ec5c30537edc9d32ff9b2
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-02-16 02:27:27 -0500

    Time: 11 ms (91.62%) | Memory: 20.1 MB (50.84%) - LeetSync

--- Trie/64-minimum-path-sum/minimum-path-sum.py
+++ Trie/64-minimum-path-sum/minimum-path-sum.py
@@ -5,15 +5,15 @@ class Solution:
 
         dp = [[0]* COLS for _ in range(ROWS)]
 
-        dp[0][0] = grid[0][0]
+        # dp[0][0] = grid[0][0]
         for i in range(1,COLS):
-            dp[0][i] = dp[0][i-1] + grid[0][i]
+            grid[0][i] = grid[0][i-1] + grid[0][i]
         
         for i in range(1,ROWS):
-            dp[i][0] = dp[i-1][0] + grid[i][0]
+            grid[i][0] = grid[i-1][0] + grid[i][0]
 
        
         for i in range(1,ROWS):
             for j in range(1,COLS):
-                dp[i][j] = grid[i][j] + min(dp[i-1][j], dp[i][j-1])
-        return dp[ROWS-1][COLS-1]
+                grid[i][j] = grid[i][j] + min(grid[i-1][j], grid[i][j-1])
+        return grid[ROWS-1][COLS-1]
