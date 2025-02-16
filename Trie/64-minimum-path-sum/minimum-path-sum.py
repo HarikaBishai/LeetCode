e4c48651f58505cbb6ba168c25f3f74ece79a9c1
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-02-16 02:25:54 -0500

    Time: 11 ms (91.62%) | Memory: 20.2 MB (50.84%) - LeetSync

--- Trie/64-minimum-path-sum/minimum-path-sum.py
+++ Trie/64-minimum-path-sum/minimum-path-sum.py
@@ -12,7 +12,7 @@ class Solution:
         for i in range(1,ROWS):
             dp[i][0] = dp[i-1][0] + grid[i][0]
 
-        
+       
         for i in range(1,ROWS):
             for j in range(1,COLS):
                 dp[i][j] = grid[i][j] + min(dp[i-1][j], dp[i][j-1])
