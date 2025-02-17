4b67f77823024b47bddfc7dae7452a0115e5b643
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-02-16 19:03:09 -0500

    Time: 7 ms (47.14%) | Memory: 17.7 MB (94.14%) - LeetSync

---
+++ Trie/415-add-strings/add-strings.py
@@ -0,0 +1,30 @@
+class Solution:
+    def addStrings(self, num1: str, num2: str) -> str:
+        i = len(num1) -1
+        j = len(num2) -1
+        carry = 0
+        sum = 0
+        out = ""
+
+        while i >= 0 or j >=0 or carry:
+            
+            sum = carry
+            if i>= 0:
+                sum+= int(num1[i])
+                i-=1
+            if j>=0:
+                sum+= int(num2[j])
+                j-=1
+            
+            if sum > 9:
+                carry = sum//10
+                
+            else:
+                carry = 0
+            
+            out = str(sum % 10) + out
+            sum = 0
+            
+
+        return out
+                
