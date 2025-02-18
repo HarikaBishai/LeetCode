aacf206e8802e1147ffc05917eda0523554796f2
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-02-18 02:14:49 -0500

    Time: 424 ms (20.13%) | Memory: 19.9 MB (85.49%) - LeetSync

--- Trie/472-concatenated-words/concatenated-words.py
+++ Trie/472-concatenated-words/concatenated-words.py
@@ -11,7 +11,8 @@ class Solution:
 
             for i in range(1,len(word)+1):
                 for j in range(i):
-                    if dp[j] and word[j:i] in words_set and word[j:i]!=word:
+                    curr_word = word[j:i]
+                    if dp[j] and curr_word in words_set and curr_word!=word:
                         dp[i] = True
                         break
             if dp[len(word)]:
