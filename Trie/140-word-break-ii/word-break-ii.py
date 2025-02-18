0f867a78ff4247081dd3b01bcad06a43cfa5c4df
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-02-17 22:11:28 -0500

    Time: 0 ms (100.00%) | Memory: 17.7 MB (67.37%) - LeetSync

--- Trie/140-word-break-ii/word-break-ii.py
+++ Trie/140-word-break-ii/word-break-ii.py
@@ -1,17 +1,22 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
-        
         out = []
-        def dfs(i,stk):
+        stk = []
+        words_set = set(wordDict) 
+        def dfs(i):
             if i == len(s):
-                out.append(" ".join(stk))
+                out.append(" ".join(stk.copy()))
+                
                 return
             
             for word in wordDict:
-                if i+len(word) <= len(s) and s[i: i+len(word)] == word:
+                if i + len(word) -1 < len(s) and s[i: i + len(word)] == word:
                     stk.append(word)
-                    dfs(i+len(word), stk)
+                    dfs(i + len(word))
                     stk.pop()
             return
-        dfs(0, [])
+        dfs(0)
         return out
+                
+
+            
