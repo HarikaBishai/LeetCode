85c15f5fe982841edfccae0efd1cac9fcae5b387
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-02-17 23:29:58 -0500

    Time: 0 ms (100.00%) | Memory: 18 MB (22.39%) - LeetSync

--- Trie/140-word-break-ii/word-break-ii.py
+++ Trie/140-word-break-ii/word-break-ii.py
@@ -2,6 +2,52 @@ class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
         out = []
         stk = []
+
+        memo = {"": [""]}
+        words_set = set(wordDict)
+
+        def dfs(remaining_str):
+            
+            if remaining_str in memo:
+                return memo[remaining_str]
+            print(remaining_str, '-----')
+          
+
+            sentences = []
+
+            for i in range(1, len(remaining_str)+1):
+                
+                    
+                curr_word = remaining_str[:i]
+                print(curr_word)
+                if curr_word in words_set:
+                    sub_setences =  dfs(remaining_str[i:])
+                    print(sub_setences)
+                    for sub in sub_setences:
+                        if sub:
+                            sentences.append(curr_word + " " + sub)
+                        else:
+                            sentences.append(curr_word)
+            memo[remaining_str] = sentences
+
+            return  memo[remaining_str]
+        dfs(s)
+
+        print(memo)
+        return memo[s]
+
+
+
+
+        def dfs(i):
+            if i in memo:
+                return memo[i]
+            sentences = []
+            for word in wordDict:
+                if i + len(word) -1 < len(s) and s[i: i + len(word)] == word:
+                    sub_sentence = dfs(i + len(word))
+                    sentences+= sub_sentence
+
         def dfs(i):
             if i == len(s):
                 out.append(" ".join(stk.copy()))
