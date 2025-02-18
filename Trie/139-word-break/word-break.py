2eda312b3aeb70a12914142e2e08226e12edabe0
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-02-17 21:56:50 -0500

    Time: 4 ms (41.92%) | Memory: 18.6 MB (5.57%) - LeetSync

--- Trie/139-word-break/word-break.py
+++ Trie/139-word-break/word-break.py
@@ -1,6 +1,47 @@
+class Node:
+    def __init__(self):
+        self.children = {}
+        self.end_of_word = False
+
+class Trie:
+    def __init__(self):
+        self.root = Node()
+    
+    def add_words(self, words):
+        for word in words:
+            curr = self.root
+            for c in word:
+                if c not in curr.children:
+                    curr.children[c] = Node()
+                curr = curr.children[c]
+            curr.end_of_word = True
+
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-    
+        
+        trie = Trie()
+        trie.add_words(wordDict)
+        root = trie.root
+
+        n = len(s)
+
+        dp = [False]* (n)
+        
+        for i in range(n):
+            if i == 0 or dp[i-1]:
+                curr = root
+                for j in range(i, n):
+                    
+                    if s[j] not in curr.children:
+                        break
+                    curr = curr.children[s[j]]
+                    if curr.end_of_word:
+                        dp[j] = True
+
+                   
+        return dp[n-1]
+
+
         n = len(s)
         dp = [False]*(n+1)
         dp[0] = True
@@ -13,7 +54,6 @@ class Solution:
         return dp[n]
         
 
-
         def dfs(i):
             if i == len(s):
                 return True
@@ -23,7 +63,6 @@ class Solution:
                     if dfs(i+len(word)):
                         return True
             
-
             return False
         
         return dfs(0)
