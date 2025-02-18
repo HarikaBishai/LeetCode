9f3f9c5c69a0987826cc2034d057fe9a64cd5bb2
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-02-17 20:48:39 -0500

    Time: 3 ms (69.53%) | Memory: 18.1 MB (13.05%) - LeetSync

--- Trie/139-word-break/word-break.py
+++ Trie/139-word-break/word-break.py
@@ -1,39 +1,29 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+    
         n = len(s)
-        dp = [False] * (n+1)
-
+        dp = [False]*(n+1)
         dp[0] = True
         wordDict = set(wordDict)
-
-        for i in range(1, n+1):
+        for i in range(1,n+1):
             for j in range(i):
-                if dp[j] and s[j:i] in wordDict:
+                if dp[j] and s[j: i] in wordDict:
                     dp[i] = True
                     break
-        return dp[-1]
+        return dp[n]
+        
 
 
+        def dfs(i):
+            if i == len(s):
+                return True
+            
+            for word in wordDict:
+                if i + len(word) -1 < len(s) and s[i: i+len(word)] == word:
+                    if dfs(i+len(word)):
+                        return True
+            
 
-
-        # q = deque([0])
-
-
-        # wordDict = set(wordDict)
-        # seen = set([0])
-
-        # while q:
-        #     for _ in range(len(q)):
-        #         start = q.popleft()
-
-        #         if start == len(s):
-        #             return True
-
-
-        #         for word in wordDict:
-        #             if start+len(word)<= len(s) and  s[start: start+len(word)] in wordDict and start+len(word) not in seen:
-        #                 q.append(start+len(word))
-        #                 seen.add(start+len(word))
-
-        # return False
-
+            return False
+        
+        return dfs(0)
