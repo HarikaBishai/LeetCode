f08f17314381eff9ea5b998ddafbad485d2ee530
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-02-19 02:22:01 -0500

    Time: 7 ms (33.09%) | Memory: 19.9 MB (75.83%) - LeetSync

--- Trie/253-meeting-rooms-ii/meeting-rooms-ii.py
+++ Trie/253-meeting-rooms-ii/meeting-rooms-ii.py
@@ -1,13 +1,15 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
         rooms = 0
-        intervals.sort()
-        heap = []
 
-        for start, end in intervals:
-            if not heap or heap[0] > start:
-                heapq.heappush(heap,end)
+        h = []
+
+        intervals.sort()
+        for st, end in intervals:
+            if not h or h[0] > st:
+                heapq.heappush(h, end)
                 rooms+=1
             else:
-                heapq.heappushpop(heap, end)
+                heapq.heappushpop(h, end)
+
         return rooms
