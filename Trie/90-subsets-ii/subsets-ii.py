88fcda126ec9d1420fb4a76ea2f4ee6cb867b795
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-02-18 21:04:16 -0500

    Time: 0 ms (100.00%) | Memory: 18.1 MB (37.90%) - LeetSync

--- Trie/90-subsets-ii/subsets-ii.py
+++ Trie/90-subsets-ii/subsets-ii.py
@@ -1,21 +1,21 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        
         stk = []
         out = []
         nums.sort()
-        def getSubSets(i):
-            if i == len(nums):
-                out.append(stk[:])
-                return
+        def dfs(i):
 
-            stk.append(nums[i])
-            getSubSets(i+1)
-            stk.pop()
-            while i+1 < len(nums) and nums[i] == nums[i+1]:
-                i = i + 1
-            
-            getSubSets(i+1)
-        
-        getSubSets(0)
+            out.append(stk.copy())
+
+            for j in range(i,len(nums)):
+                if j != i and nums[j] == nums[j-1]:
+                    continue
+                stk.append(nums[j])
+                
+                dfs(j+1)
+
+                stk.pop()
+                
+                    
+        dfs(0)
         return out
