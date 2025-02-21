6474fcebfb9469691274b908103f04e96a08ca00
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-02-21 11:34:24 -0500

    Time: 284 ms (45.41%) | Memory: 76.4 MB (43.58%) - LeetSync

--- Trie/2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.py
+++ Trie/2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.py
@@ -13,24 +13,26 @@ class Solution:
         visited = set([1])
 
 
-       
+        out = float('inf')
+        def dfs(node):
+            nonlocal out
+            for nei, dist in graph[node]:
+                out = min(dist, out)
+                if nei not in visited:
+                    visited.add(nei)
+                    dfs(nei)
+        dfs(1)
+        return out
 
         
         out = float('inf')
         while q:
             city = q.popleft()
 
-            
-
-            # if city in visited:
-            #     continue
-            # visited.add(city)
-
 
             for nei, sc in graph[city]:
                 out = min(sc, out)
                 if nei not in visited:
-                    
                     q.append(nei)
                     visited.add(nei)
                 
