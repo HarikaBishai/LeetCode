e0e93ee50cc8de9ee79f411d8090687c38034ceb
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-02-21 16:54:47 -0500

    Time: 546 ms (5.04%) | Memory: 53.9 MB (22.32%) - LeetSync

---
+++ Trie/1492-time-needed-to-inform-all-employees/time-needed-to-inform-all-employees.py
@@ -0,0 +1,26 @@
+class Solution:
+    def numOfMinutes(self, n: int, headID: int, manager: List[int], informTime: List[int]) -> int:
+        
+        graph = defaultdict(list)
+        graph[headID] = []
+        for employee, manager in enumerate(manager):
+            if manager!= -1:
+                inform_time = informTime[manager]
+                graph[manager].append((employee, inform_time))
+        print(graph)
+
+
+        q = deque([(headID, 0)])
+        time = 0
+
+        while q:
+            for _ in range(len(q)):
+                employee,m_time = q.popleft()
+                time = max(time, m_time)
+
+                
+                # if employee not in graph:
+                #     continue
+                for nei, nei_time in graph[employee]:
+                    q.append((nei, m_time + nei_time))
+        return time
