64ed79deed5a5d2eea686a56f5b5ab2c1d951761
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-02-21 01:10:57 -0500

    Time: 374 ms (97.36%) | Memory: 19 MB (30.62%) - LeetSync

--- Trie/322-coin-change/coin-change.py
+++ Trie/322-coin-change/coin-change.py
@@ -1,21 +1,19 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        
-        q = deque([0])
         seen = set([0])
-        level = 0
+        q = deque([[0, 0]])
+
         while q:
-            for i in range(len(q)):
-                start = q.popleft()
-
-                if start == amount:
-                    return level
-
-                for coin in coins:
-                    if start + coin <= amount and start + coin not in seen:
-                        q.append(start+coin)
-                        seen.add(start + coin)
-
-            level+=1
-
+            sum, n_coins= q.popleft()
+            if sum == amount:
+                return n_coins
+            for coin in coins:
+                if coin + sum <= amount and coin + sum not in seen:
+                    seen.add(coin + sum)
+                    q.append([coin + sum, n_coins+1])
         return -1
+
+        
+
+        
+
