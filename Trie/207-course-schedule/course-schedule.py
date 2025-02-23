91242f5fc7e94007172584d4993a58fed442e402
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-02-23 18:25:52 -0500

    Time: 7 ms (49.59%) | Memory: 19 MB (83.58%) - LeetSync

--- Trie/207-course-schedule/course-schedule.py
+++ Trie/207-course-schedule/course-schedule.py
@@ -1,27 +1,26 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        
-        indegree = {i: 0 for i in range(numCourses)}
-        graph = {i:[] for i in range(numCourses)}
+        indegree = {i:0 for i in range(numCourses)}
+
+        graph = defaultdict(list)
 
         for v, u in prerequisites:
             graph[u].append(v)
             indegree[v]+=1
-            
+        
+
         q = deque()
         for node in indegree:
             if indegree[node] == 0:
                 q.append(node)
-
+        
         while q:
             node = q.popleft()
-            numCourses -= 1
+            
+            numCourses-=1
 
             for nei in graph[node]:
-                indegree[nei] -= 1
+                indegree[nei]-=1
                 if indegree[nei] == 0:
                     q.append(nei)
-                    
         return True if numCourses == 0 else False
-
-
