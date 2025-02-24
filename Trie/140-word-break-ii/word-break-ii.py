d498c1566813d5911ec73a1904b763e269224951
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-02-24 18:38:05 -0500

    Time: 0 ms (100.00%) | Memory: 17.7 MB (90.94%) - LeetSync

--- Trie/140-word-break-ii/word-break-ii.py
+++ Trie/140-word-break-ii/word-break-ii.py
@@ -1,61 +1,24 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
-
-        memo = {"": [""]}
+        
+        out = []
+        dp = {"": [""]}
         words_set = set(wordDict)
 
-        def dfs(remaining_str):
-            
-            if remaining_str in memo:
-                return memo[remaining_str]
-          
+        def dfs(s):
+            if s in dp:
+                return dp[s]
             sentences = []
 
-            for i in range(1, len(remaining_str)+1):   
-                curr_word = remaining_str[:i]
+            for i in range(1, len(s) +1):
+                curr_word = s[:i]
                 if curr_word in words_set:
-                    sub_setences =  dfs(remaining_str[i:])
-                    for sub in sub_setences:
+                    sub_sentences = dfs(s[i:])
+                    for sub in sub_sentences:
                         if sub:
                             sentences.append(curr_word + " " + sub)
                         else:
                             sentences.append(curr_word)
-            memo[remaining_str] = sentences
-
-            return  memo[remaining_str]
-        dfs(s)
-
-        return memo[s]
-
-
-
-
-
-
-
-        def dfs(i):
-            if i in memo:
-                return memo[i]
-            sentences = []
-            for word in wordDict:
-                if i + len(word) -1 < len(s) and s[i: i + len(word)] == word:
-                    sub_sentence = dfs(i + len(word))
-                    sentences+= sub_sentence
-
-        def dfs(i):
-            if i == len(s):
-                out.append(" ".join(stk.copy()))
-                
-                return
-            
-            for word in wordDict:
-                if i + len(word) -1 < len(s) and s[i: i + len(word)] == word:
-                    stk.append(word)
-                    dfs(i + len(word))
-                    stk.pop()
-            return
-        dfs(0)
-        return out
-                
-
-            
+            dp[s] = sentences
+            return dp[s]
+        return dfs(s)
