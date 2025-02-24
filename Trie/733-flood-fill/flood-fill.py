50620830ffd358efcbf88740f2776008eaa0ffab
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-02-24 14:14:37 -0500

    Time: 0 ms (100.00%) | Memory: 17.9 MB (88.53%) - LeetSync

--- Trie/733-flood-fill/flood-fill.py
+++ Trie/733-flood-fill/flood-fill.py
@@ -12,16 +12,18 @@ class Solution:
         if target == color:
             return image
 
+        image[sr][sc] = color
         while q:
+
             r, c = q.popleft()
             
-            image[r][c] = color
             dir = [(-1,0),(1,0),(0,1),(0,-1)]
 
             for i , j in dir:
                 new_r = i+r
                 new_c = j+c
                 if new_r in range(ROWS) and new_c in range(COLS) and image[new_r][new_c] == target:
+                    image[new_r][new_c] = color
                     q.append((new_r, new_c))
 
         return image
