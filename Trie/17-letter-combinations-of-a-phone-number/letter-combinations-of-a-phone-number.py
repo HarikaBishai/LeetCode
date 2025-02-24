663fef69c20fe6bb14b07304502dd1ac80442dfa
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-02-24 15:08:43 -0500

    Time: 0 ms (100.00%) | Memory: 17.8 MB (68.66%) - LeetSync

--- Trie/17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ Trie/17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -4,6 +4,26 @@ class Solution:
         mapping = {'2':'abc', '3':'def','4':'ghi','5':'jkl','6':'mno','7':'pqrs','8':'tuv','9':'wxyz'}
 
 
+        dp = {"": [""]}
+
+        def dfs(digits):
+            if digits in dp:
+                return dp[digits]
+            first_digit = digits[0]
+            remaining_digits = digits[1:]
+            remaining_sub = dfs(remaining_digits)
+            out = []
+            for digit in mapping[first_digit]:
+                for sub in remaining_sub:
+                    
+                    out.append(digit + sub)
+                    
+
+            dp[digits] = out
+            return out
+        out = dfs(digits) 
+        return out if out!=[""] else []
+
         stk = []
         out = []
         def dfs(i, stk):
@@ -22,44 +42,7 @@ class Solution:
 
 
 
-        # dp = {"":[]}
-
-        # def backtrack(numstring):
-        #     if numstring in dp:
-        #         return dp[numstring]
-
-        #     curr_letters = list(mapping[numstring[0]])
-        #     for i in range(1, len(numstring)):
-        #         remaining_str = numstring[i:]
-        #         out = []
-        #         sub_str = backtrack(remaining_str)
-        #         for sub in sub_str:
-        #             for curr in curr_letters:
-        #                 out.append(curr+sub)
-        #         curr_letters = out
-        #     dp[numstring] = curr_letters
-        #     return curr_letters
-
-        # return backtrack(digits)
-         
-
-
-
-        def backtrack(numstring, comb_string):
-            if not numstring:
-                return
-            nonlocal letter_comb_list
-            num = numstring[0]
-            string = mapping[num]
-
-            for i in string:
-                if len(numstring) > 1:
-                    backtrack(numstring[1:],comb_string+i)
-                else:
-                    letter_comb_list.append(comb_string+i)
-
-        backtrack(digits, "")
-        return letter_comb_list
+        
 
             
 
