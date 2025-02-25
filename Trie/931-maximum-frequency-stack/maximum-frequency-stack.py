c8974000277a08d0e1f2a498b1cfb735121517c6
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-02-24 22:05:50 -0500

    Time: 65 ms (86.49%) | Memory: 26.3 MB (81.54%) - LeetSync

--- Trie/931-maximum-frequency-stack/maximum-frequency-stack.py
+++ Trie/931-maximum-frequency-stack/maximum-frequency-stack.py
@@ -1,22 +1,32 @@
 class FreqStack:
 
     def __init__(self):
-        self.groups = defaultdict(list)
-        self.counter = defaultdict(int)
-        self.max = 0
+        self.stk = {}
+        self.count = 0
+        self.counter = {}
+
     def push(self, val: int) -> None:
-        self.counter[val] +=1
-        self.groups[self.counter[val]].append(val)
-        if self.max < self.counter[val]:
-            self.max = self.counter[val]
+        if val not in self.counter:
+            self.counter[val] = 1
+        else:
+            self.counter[val] += 1
+        
+        if self.counter[val] not in self.stk:
+            self.stk[self.counter[val]] = []
+        self.stk[self.counter[val]].append(val)
+        self.count = max(self.count, self.counter[val])
     def pop(self) -> int:
-        ele = self.groups[self.max].pop()
-
-        if not self.groups[self.max]:
-            self.max -= 1
-        self.counter[ele]-=1
-        return ele
-
+        if not self.stk:
+            return -1
+        else:
+            ele = self.stk[self.count].pop()
+            if not self.stk[self.count]:
+                del self.stk[self.count]
+                self.count-=1
+            self.counter[ele]-=1
+            if self.counter[ele] == 0:
+                del self.counter[ele]
+            return ele
 
 
 # Your FreqStack object will be instantiated and called as such:
