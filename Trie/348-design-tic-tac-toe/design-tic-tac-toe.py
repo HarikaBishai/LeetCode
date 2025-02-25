2fe69321be30af08816256c72128d65dd5277f60
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-02-24 20:21:52 -0500

    Time: 79 ms (56.69%) | Memory: 20.2 MB (73.55%) - LeetSync

--- Trie/348-design-tic-tac-toe/design-tic-tac-toe.py
+++ Trie/348-design-tic-tac-toe/design-tic-tac-toe.py
@@ -1,40 +1,61 @@
 class TicTacToe:
 
     def __init__(self, n: int):
-        self.board = [[0]*n for _ in range(n)]
+        # self.board = [[0]*n for _ in range(n)]
+        # self.n = n
+        self.rows = [0]*n
+        self.cols = [0]*n
+        self.diag = 0
+        self.anti_diag = 0
         self.n = n
 
 
-        
-
     def move(self, row: int, col: int, player: int) -> int:
-        self.board[row][col] = player
+        n = self.n
+        curr_value = 1 if player == 1 else -1
 
-        def check_row():
-            for i in range(self.n):
-                if self.board[row][i]!= player:
-                    return False
-            return True
+        self.rows[row] += curr_value
+        self.cols[col] += curr_value
+
+        if row == col:
+            self.diag += curr_value
         
-        def check_col():
-            for i in range(self.n):
-                if self.board[i][col]!= player:
-                    return False
-            return True
+        if col == n - row -1:
+            self.anti_diag += curr_value
+
         
-        def check_left_diag():
-            for i in range(self.n):
-                if self.board[i][i]!= player:
-                    return False
-            return True
+        if abs(self.rows[row]) == n or abs(self.cols[col]) == n or abs(self.diag) == n or abs(self.anti_diag) == n:
+            return player
+        return 0
+
+
+        # self.board[row][col] = player
+
+        # def check_row():
+        #     for i in range(self.n):
+        #         if self.board[row][i]!= player:
+        #             return False
+        #     return True
         
-        def check_right_diag():
-            for i in range(self.n):
-                if self.board[i][self.n-i-1]!= player:
-                    return False
-            return True
+        # def check_col():
+        #     for i in range(self.n):
+        #         if self.board[i][col]!= player:
+        #             return False
+        #     return True
         
-        return player if check_row() or check_col() or check_left_diag() or check_right_diag() else 0
+        # def check_left_diag():
+        #     for i in range(self.n):
+        #         if self.board[i][i]!= player:
+        #             return False
+        #     return True
+        
+        # def check_right_diag():
+        #     for i in range(self.n):
+        #         if self.board[i][self.n-i-1]!= player:
+        #             return False
+        #     return True
+        
+        # return player if check_row() or check_col() or check_left_diag() or check_right_diag() else 0
         
 
         
