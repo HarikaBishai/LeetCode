f89f511fe4c68b3f5b2468e015940167b34c84d8
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-03-07 16:24:59 -0500

    Time: 73 ms (53.81%) | Memory: 33.2 MB (31.57%) - LeetSync

--- Trie/3151-minimum-processing-time/minimum-processing-time.py
+++ Trie/3151-minimum-processing-time/minimum-processing-time.py
@@ -4,18 +4,16 @@ class Solution:
         n = len(tasks)
         out = 0
         processorTime.sort(key=lambda x: -x)
-
         tasks.sort()
         l = 0
         i = 0
         while l < len(tasks):
             if l + 3 < n:
                 l = l+3
-                out = max(tasks[l]+processorTime[i], out)
-                i +=1
+                
             else:
                 l = n-1
-                out = max(tasks[l]+processorTime[i], out)
-                i+=1
+            out = max(tasks[l]+processorTime[i], out)
+            i+=1
             l+=1
         return out
